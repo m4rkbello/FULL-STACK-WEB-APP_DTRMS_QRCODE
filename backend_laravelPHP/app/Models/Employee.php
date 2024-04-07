348382e363e64e8d2720fa2d8792cc1c804181d1
m4rkbello <markbello10041999@gmail.com>
2024-04-07 12:30:35 +0800

    BACKEND: add migration, model for employees table and attendance with FK as 1 to Many for attendance purposes using QRCODE

--- backend_laravelPHP/app/Models/Employee.php
+++ backend_laravelPHP/app/Models/Employee.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'employee_fullname',
+        'employee_email',
+        'employee_contact_no',
+        'employee_position',
+        'employee_role',
+        'employee_department',
+        'employee_status'
+    ];
+
+    public function attendances(){
+        return $this->hasMany(Attendance::class, 'attendance_employee_id');
+    }
+
 }
