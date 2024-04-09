09af6a3eca1edd6fb03614bd7b48527eb7752925
m4rkbello <markbello10041999@gmail.com>
2024-04-09 17:56:55 +0800

    FRONTEND/BACKLEND/API: with redux working

--- frontend_reactJS/src/main.jsx
+++ frontend_reactJS/src/main.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <Provider store={store}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  </React.StrictMode>,
+  </Provider>
+  </React.StrictMode>
 )
