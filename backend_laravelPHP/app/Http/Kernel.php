fc23563df58263fbb561713f7142862c57e87e98
m4rkbello <markbello10041999@gmail.com>
2024-04-09 17:53:08 +0800

    FRONTEND/BACKLEND/API: with redux working

--- backend_laravelPHP/app/Http/Kernel.php
+++ backend_laravelPHP/app/Http/Kernel.php
@@ -54,7 +54,6 @@ class Kernel extends HttpKernel
      */
     protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
-        'auth:sanctum' => \Laravel\Sanctum\Http\Middleware\EnsureTokenIsValid::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
