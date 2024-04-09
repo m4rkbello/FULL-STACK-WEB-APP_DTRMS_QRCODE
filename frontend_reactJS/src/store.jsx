94f0df11e90da88ad7e83ab509da93dd2e90c7da
m4rkbello <markbello10041999@gmail.com>
2024-04-09 17:57:06 +0800

    FRONTEND/BACKLEND/API: with redux working

--- frontend_reactJS/src/store.jsx
+++ frontend_reactJS/src/store.jsx
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers'; // Import your root reducer
+import {thunk} from 'redux-thunk';
+import rootReducer from './components/redux/rootReducers'; // Import your root reducer
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
