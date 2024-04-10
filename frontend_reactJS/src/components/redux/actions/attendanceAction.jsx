c87d6a81689fe9b849481bf1eb74f17b93819a8e
m4rkbello <markbello10041999@gmail.com>
2024-04-10 14:02:21 +0800

    FRONTEND/REDUX: add action for attendance

---
+++ frontend_reactJS/src/components/redux/actions/attendanceAction.jsx
@@ -0,0 +1,91 @@
+import MarkBelloApi from '../../../services/Api.jsx';
+// import { toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+
+import {
+    FETCH_EMPLOYEES_REQUEST,
+    FETCH_EMPLOYEES_SUCCESS,
+    FETCH_EMPLOYEES_FAILURE,
+    ADD_EMPLOYEE_REQUEST,
+    ADD_EMPLOYEE_SUCCESS,
+    ADD_EMPLOYEE_FAILURE,
+    UPDATE_EMPLOYEE_REQUEST,
+    UPDATE_EMPLOYEE_SUCCESS,
+    UPDATE_EMPLOYEE_FAILURE,
+    DELETE_EMPLOYEE_REQUEST,
+    DELETE_EMPLOYEE_SUCCESS,
+    DELETE_EMPLOYEE_FAILURE,
+
+} from '../types/employeeTypes.jsx';
+
+//MAG-FETCH UG EMPLOYEE
+export const fetchAttendances = () => async dispatch => {
+    try {
+        dispatch({ type: FETCH_EMPLOYEES_REQUEST });
+        // Perform async operation, e.g., fetch data from an API
+        const attendances = await MarkBelloApi.get('/api/employees');
+        dispatch({
+            type: FETCH_EMPLOYEES_SUCCESS,
+            payload: attendances
+        });
+    } catch (error) {
+        dispatch({
+            type: FETCH_EMPLOYEES_FAILURE,
+            payload: error.message
+        });
+    }
+};
+
+//MAG ADD UG EMPLOYEE 
+export const addAttendance = newAttendance => async dispatch => {
+    try {
+        dispatch({ type: ADD_EMPLOYEE_REQUEST });
+        // Perform async operation, e.g., send data to an API
+        const addAttendance = await MarkBelloApi.post(newAttendance);
+        dispatch({
+            type: ADD_EMPLOYEE_SUCCESS,
+            payload: addAttendance
+        });
+    } catch (error) {
+        dispatch({
+            type: ADD_EMPLOYEE_FAILURE,
+            payload: error.message
+        });
+    }
+};
+
+//MAG UPDATE UG EMPLOYEE GAMIT ID
+export const updateAttendance = (attendanceId, updatedAttendanceData) => async dispatch => {
+    try {
+        dispatch({ type: UPDATE_EMPLOYEE_REQUEST });
+        // Perform async operation, e.g., send updated data to an API
+        const updateAttendance = await MarkBelloApi.put(attendanceId, updatedAttendanceData);
+        dispatch({
+            type: UPDATE_EMPLOYEE_SUCCESS,
+            payload: updateAttendance
+        });
+    } catch (error) {
+        dispatch({
+            type: UPDATE_EMPLOYEE_FAILURE,
+            payload: error.message
+        });
+    }
+};
+
+//MAG DELETE UG EMPLOYEE
+export const deleteAttendance = attendanceId => async dispatch => {
+    try {
+        dispatch({ type: DELETE_EMPLOYEE_REQUEST });
+        // Perform async operation, e.g., send delete request to an API
+        await MarkBelloApi.delete(attendanceId);
+        dispatch({
+            type: DELETE_EMPLOYEE_SUCCESS,
+            payload: attendanceId
+        });
+    } catch (error) {
+        dispatch({
+            type: DELETE_EMPLOYEE_FAILURE,
+            payload: error.message
+        });
+    }
+};
