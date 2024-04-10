c5af7b9c6913350d5557935c487ebec5707782db
m4rkbello <markbello10041999@gmail.com>
2024-04-10 14:01:47 +0800

    FRONTEND/REDUX: configure the name path of userReducer, EmployeeReducer and Attendance

--- frontend_reactJS/src/components/redux/rootReducers.jsx
+++ frontend_reactJS/src/components/redux/rootReducers.jsx
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import userReducer from './reducers/userReducer';
+import employeeReducer from './reducers/employeeReducer';
+import attendanceReducer from './reducers/attendanceReducer';
 
 const rootReducer = combineReducers({
-    userReducer: userReducer,
+    userState: userReducer,
+    employeeState: employeeReducer,
+    attendanceState: attendanceReducer
 });
 
 export default rootReducer;
