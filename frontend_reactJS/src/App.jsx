115c326a542f07e0842d1d211ef3467960fd0f58
m4rkbello <markbello10041999@gmail.com>
2024-04-10 16:40:47 +0800

    FRONTEND: getting items form localStorage, SessionStorage and Cookies specifically the userId as to use as baseline for the collections of redux

--- frontend_reactJS/src/App.jsx
+++ frontend_reactJS/src/App.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable react-refresh/only-export-components */
 import { connect } from 'react-redux';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
 import Login from './components/Auth/admin/Login';
 import Register from './components/Auth/admin/Register';
@@ -12,15 +12,42 @@ import Content from './components/layouts/Content';
 import Footer from './components/layouts/Footer';
 import EmployeeRegister from './components/Auth/employee/EmployeeRegister';
 
-import {fetchUsers} from './components/redux/actions/userAction' 
+//redux-actions
+import { fetchUsers } from './components/redux/actions/userAction';
+import { fetchEmployees } from './components/redux/actions/employeeAction';
+import { fetchAttendances } from './components/redux/actions/attendanceAction';
 
-
-function App({fetchUsers}) {
+function App(props) {
+  //FOR AUTHENTICATION-PURPOSES
+  const [localStorageHasUserId, setLocalStorageHasUserId] = useState('');
+  const [sessionStorageHasUserId, setSessionStorageHasUserId] = useState('');
+  const [localStorageHasToken, setLocalStorageHasToken] = useState('');
+  const [sessionStorageToken, setSessionStorageToken] = useState('');
+  const [cookiesData, setCookiesData] = useState('');
 
   useEffect(() => {
-    fetchUsers();
-  }, [fetchUsers]);
+    //kuhaon ang data sa localStorage/Session Storage/Cookie 
+    const localStorageHasUserId = localStorage.getItem('DTRMS_BY_M4RKBELLO_USER_ID');
+    const sessionStorageHasUserId = sessionStorage.getItem('DTRMS_BY_M4RKBELLO_USER_ID');
+    const localStorageHasTokenData = localStorage.getItem('DTRMS_BY_M4RKBELLO');
+    const sessionStorageHasTokenData = sessionStorage.getItem('DTRMS_BY_M4RKBELLO');
 
+    const cookiesData = document.cookie;
+
+    setLocalStorageHasUserId(localStorageHasUserId);
+    setSessionStorageHasUserId(sessionStorageHasUserId);
+    setLocalStorageHasToken(localStorageHasTokenData);
+    setSessionStorageToken(sessionStorageHasTokenData);
+    setCookiesData(cookiesData);
+
+    console.log("ID SA LOCALSTORAGE", localStorageHasUserId);
+    console.log("ID SA SESSION STORAGE", sessionStorageHasUserId);
+    console.log("ID SA COOKIE", cookiesData);
+
+    props.fetchUsers();
+    props.fetchEmployees();
+    props.fetchAttendances();
+  }, []);
 
 
 
@@ -47,80 +74,126 @@ function App({fetchUsers}) {
         <div className="flex-1">
           <a className="btn btn-ghost text-2xl text-black">DTRMS BY M4RKBELLO</a>
         </div>
-        <div className="dropdown dropdown-end">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-              />
+
+        {(localStorageHasToken?.length ?? 0) > 0 && (sessionStorageToken?.length ?? 0) !== 0 && (cookiesData?.length ?? 0) > 0 ? (
+          <>
+            <div className="dropdown dropdown-end">
+              <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS Navbar component"
+                    src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                  />
+                </div>
+              </div>
+              <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-amber-100 rounded-box w-52">
+                <li>
+                  <a className="justify-between text-black">
+                    Profile <span className="badge bg-black"><span className='text-white'>Open</span></span>
+                  </a>
+                </li>
+                <li className='text-black'>
+                  <a>Settings</a>
+                </li>
+                <li className='text-black'>
+                  <a>Logout</a>
+                </li>
+              </ul>
             </div>
-          </div>
-          <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-amber-100 rounded-box w-52">
-            <li>
-              <a className="justify-between text-black">
-                Profile <span className="badge bg-black"><span className='text-white'>Open</span></span>
-              </a>
-            </li>
-            <li className='text-black'>
-              <a>Settings</a>
-            </li>
-            <li className='text-black'>
-              <a>Logout</a>
-            </li>
-          </ul>
-        </div>
+          </>
+        ) : (
+          <>
+            <div>
+              <ul className="menu menu-horizontal px-1 text-amber-100 bg-black">
+                <li className='shadow-2xl'>
+                  <Link to="/qrc">Scan QR</Link>
+                </li>
+                <li className='shadow-2xl'>
+                  <Link to="/admin/login">Login</Link>
+                </li>
+                <li className='shadow-2xl'>
+                  <Link to="/admin/register">Register</Link>
+                </li>
+                <li className='shadow-2xl'>
+                </li>
+              </ul>
+            </div>
+          </>
+        )}
       </div>
 
       <div className="drawer lg:drawer-open flex-1">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col items-center justify-center px-4 py-8 md:px-8 md:py-12">
-          <Routes>
-            <Route path="/admin/login" element={<Login />} />
-            <Route path="/admin/register" element={<Register />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/content" element={<Content />} />
-            <Route path="/details" element={<PersonalDetails />} />
-            <Route path="/register" element={<EmployeeRegister />} />
-          </Routes>
-        </div>
-        <div className="drawer-side">
-          <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-          <ul className="menu pt-4 pl-4 pr-4 pb-4 w-80 min-h-full bg-black text-amber-100">
-            <li>
-              <Link to="/admin/login">
-                Login Testing
-              </Link>
-            </li>
-            <li>
-              <Link to="/content">
-                Content Test
-              </Link>
-            </li>
-            <li>
-              <Link to="/details">
-                Details Test
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
+          {(localStorageHasToken?.length ?? 0) > 0 && (sessionStorageToken?.length ?? 0) !== 0 && (cookiesData?.length ?? 0) > 0 ?
+            (
+              <>
+                <Routes>
+                  <Route path="/forgot-password" element={<ForgotPassword />} />
+                  <Route path="/content" element={<Content />} />
+                  <Route path="/details" element={<PersonalDetails />} />
+                  <Route path="/register" element={<EmployeeRegister />} />
+                </Routes>
+              </>
+            ) : (
+              <Routes>
+                <Route path="/admin/login" element={<Login />} />
+                <Route path="/admin/register" element={<Register />} />
+              </Routes>
+            )}
 
+        </div>
+
+        {(localStorageHasToken?.length ?? 0) > 0 && (sessionStorageToken?.length ?? 0) !== 0 && (cookiesData?.length ?? 0) > 0 ? (
+          <>
+          <div className="drawer-side">
+            <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
+            <ul className="menu pt-4 pl-4 pr-4 pb-4 w-80 min-h-full bg-black text-amber-100">
+              <li>
+                <Link to="/admin/login">
+                  Login Testing
+                </Link>
+              </li>
+              <li>
+                <Link to="/content">
+                  Content Test
+                </Link>
+              </li>
+              <li>
+                <Link to="/details">
+                  Details Test
+                </Link>
+              </li>
+            </ul>
+          </div>
+          </>
+        ) : (
+          <>
+
+          </>
+        )}
+      </div>
       <Footer />
     </div>
   );
 }
 
 const mapStateToProps = (state) => {
-  console.log("DATA SA MAPTOSTATETOPROPS", state);
+  console.log("DATA SA MAPTOSTATETOPROPS users", state.userState);
+  console.log("DATA SA MAPTOSTATETOPROPS employees", state.employeeState);
+  console.log("DATA SA MAPTOSTATETOPROPS attendances", state.attendanceState);
   return {
-    users: state.users
+    users: state.users,
+    employees: state.employees,
+    attendances: state.attendances
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     fetchUsers: () => dispatch(fetchUsers()),
+    fetchEmployees: () => dispatch(fetchEmployees()),
+    fetchAttendances: () => dispatch(fetchAttendances()),
   };
 };
 
