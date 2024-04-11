0433c2903a01d3994faef4d38a1050993d0d48b2
m4rkbello <markbello10041999@gmail.com>
2024-04-11 23:32:04 +0800

    BACKEND: add Images using eloquent and foreignkey with user.id as img_user_id and employees as img_emp_id

---
+++ backend_laravelPHP/database/migrations/2024_04_11_142908_create_images_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('img_name')->nullable();
+            $table->integer('img_status_id');
+            $table->unsignedBigInteger('img_user_id'); // Use unsignedBigInteger for foreign keys
+            $table->unsignedBigInteger('img_emp_id'); // Use unsignedBigInteger for foreign keys
+            $table->foreign('img_user_id')->references('id')->on('users');
+            $table->foreign('img_emp_id')->references('id')->on('employees');
+            $table->timestamps();
+        });
+
+    }
+        
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('images');
+    }
+};
