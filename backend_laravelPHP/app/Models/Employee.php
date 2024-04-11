b442875a526f4e199fe61614ea7dd098f22c8fe9
m4rkbello <markbello10041999@gmail.com>
2024-04-11 23:26:26 +0800

    BACKEND: add Images using eloquent and foreignkey with user.id as img_user_id and employees as img_emp_id

--- backend_laravelPHP/app/Models/Employee.php
+++ backend_laravelPHP/app/Models/Employee.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\HasMany;
 
 class Employee extends Model
 {
@@ -22,5 +24,11 @@ class Employee extends Model
     public function attendances(){
         return $this->hasMany(Attendance::class, 'attendance_employee_id');
     }
+
+    public function images(){
+        return $this->hasMany(Images::class, 'img_emp_id');
+    }
+
+    
 
 }
