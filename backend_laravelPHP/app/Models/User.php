bae077815ac235cd088444a16bacba7145bdfaeb
m4rkbello <markbello10041999@gmail.com>
2024-04-11 23:30:54 +0800

    BACKEND: add Images using eloquent and foreignkey with user.id as img_user_id and employees as img_emp_id

--- backend_laravelPHP/app/Models/User.php
+++ backend_laravelPHP/app/Models/User.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\BelongsTo;
+
 
 class User extends Authenticatable
 {
@@ -44,4 +47,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+
+    public function images(){
+        return $this->hasMany(Image::class, 'img_user_id');
+    }
 }
