c618f59b5a03a3fa7e4ccf4f9824bc3eec4ccace
m4rkbello <markbello10041999@gmail.com>
2024-04-13 15:52:25 +0800

    FRONTEND: employee dashboard - fixed

--- frontend_reactJS/src/components/Auth/employee/EmployeeDashboard.jsx
+++ frontend_reactJS/src/components/Auth/employee/EmployeeDashboard.jsx
@@ -1,76 +1,122 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
+
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchEmployees } from '../../redux/actions/employeeAction';
+import { useEffect } from 'react';
 
 
 
 
-import { Link  } from 'react-router-dom';
+const EmployeeDashboard = (props) => {
+    // const { employees } = props;
 
 
-const EmployeeDashboard = () => {
+
+    useEffect(() => {
+        props.fetchEmployees();
+    }, []);
+
+
+    const employeesCollectionArrays = props.employeesData?.employees?.data;
+    console.log("DATA SA EMPLOYEES", employeesCollectionArrays);
+
+
+    function getAllEmployees(employeesCollectionArrays) {
+        let item = [];
+
+        if (employeesCollectionArrays) {
+            for (let ez = 0; ez < employeesCollectionArrays.length; ez++) {
+
+                item.push(employeesCollectionArrays[ez]);
+            }
+        }
+        return item;
+
+    }
+
+    const employeesList = getAllEmployees(employeesCollectionArrays);
+
+    // console.log("DATA SA EMPLOYEE NA GI PUSH!", employeesList);
+
     return (
         <div className="hero max-w-full">
             <div className="hero min-h-screen bg-amber-100 rounded-lg">
                 <div className="pt-0 pl-0 pr-0 pb-0 my-0 mx-0 px-0 py-0">
-                    <span className="text-3xl text-black">
-                    <center>
-                    EMPLOPYEE DASHBOARD
-                    </center>
+                    <span className="text-4xl font-black text-black b">
+                        <center>
+                            EMPLOPYEE DASHBOARD
+                        </center>
                     </span>
-                   
+
                     <div className="overflow-x-auto bg-transparent text-black">
-                        <table className="table py-10 px-10 my-10 mx-10">
-                            {/* head */}
-                            <thead className="text-black text-1xl">
-                                <tr>
-                                    <th>Avatar</th>
-                                    <th>Fullname</th>
-                                    <th>Email</th>
-                                    <th>Contact No.</th>
-                                    <th>Position</th>
-                                    <th>Department</th>
-                                    <th>Status</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
+                    {Array.isArray(employeesCollectionArrays) && employeesCollectionArrays.length > 0 ? (
+                            <table className="table py-10 px-10 my-10 mx-10">
+                                {/* head */}
+                                <thead className="text-black text-1xl">
+                                    <tr>
+                                        <th>Avatar</th>
+                                        <th>Fullname</th>
+                                        <th>Email</th>
+                                        <th>Contact No.</th>
+                                        <th>Position</th>
+                                        <th>Department</th>
+                                        <th>Status</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
 
-                                <tr>
-                                    <td>
-                                        <div className="flex items-center gap-3">
-                                            <div className="avatar">
-                                                <div className="mask mask-squircle w-12 h-12">
-                                                    <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                                    {employeesList && employeesList.map((item, index) => (
+                                        <tr key={index}>
+                                            <td>
+                                                <div className="flex items-center gap-3">
+                                                    <div className="avatar">
+                                                        <div className="mask mask-squircle w-12 h-12">
+                                                            <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                                                        </div>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td>
-                                       2
-                                    </td>
-                                    <td>3</td>
-                                    <td>
-                                    4
-                                    </td>
-                                    <td>5
-                                    </td>
-                                    <td>
-                                    6
-                                    </td>
-                                    <td>
-                                    7
-                                    </td>
-                                    <td>
-                                    <button className="btn btn-primary p-5 m-2">
-                                    <Link to="/register">View</Link>
-                                  </button>
-                          
-                                    <button className="btn btn-primary p-5 m-2">Remove</button>
-                                   
-                                    </td>
-                                </tr>
-                            </tbody>
+                                            </td>
+                                            <td>
+                                                {item.employee_fullname}
+                                            </td>
+
+                                            <td>
+                                                {item.employee_email}
+                                            </td>
+
+                                            <td>
+                                                {item.employee_contact_no}
+                                            </td>
+                                            <td>5
+                                            </td>
+                                            <td>
+                                                {item.employee_position}
+                                            </td>
+                                            <td>
+                                                {item.employee_role}
+                                            </td>
+                                            <td>
+                                                <button className="btn btn-primary p-5 m-2">
+                                                    <Link to="/register">View</Link>
+                                                </button>
+
+                                                <button className="btn btn-primary p-5 m-2">Remove</button>
+
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
 
 
-                        </table>
+                            </table>
+
+                        ) : (
+                            <h1>NO DATA</h1>
+                        )}
+
                     </div>
                 </div>
 
@@ -79,7 +125,23 @@ const EmployeeDashboard = () => {
     )
 }
 
+const mapStateToProps = (state) => {
+    console.log("DATA SA MAPTOSTATETOPROPS e", state.employeeState);
+    return {
+        employeesData: state.employeeState,
+    };
+};
 
-export default EmployeeDashboard;
+const mapDispatchToProps = (dispatch) => {
+    return {
+
+        fetchEmployees: () => dispatch(fetchEmployees()),
+
+    };
+};
+
+
+// eslint-disable-next-line react-refresh/only-export-components
+export default connect(mapStateToProps, mapDispatchToProps)(EmployeeDashboard);
 
 
