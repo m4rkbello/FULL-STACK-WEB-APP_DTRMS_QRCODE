01ef4545a62c5b9029567552e24fb865249f3286
m4rkbello <markbello10041999@gmail.com>
2024-04-14 15:54:53 +0800

    API: invoking the token as the validation to accessthe middleware routes

--- frontend_reactJS/src/services/Api.jsx
+++ frontend_reactJS/src/services/Api.jsx
@@ -1,9 +1,37 @@
+/* eslint-disable react-refresh/only-export-components */
 import axios from 'axios';
+
 
 const Api = axios.create({
   baseURL: "http://127.0.0.1:8000/",
   withCredentials: true
 });
+
+
+export const getTokenFromLocalStorage = () => {
+  return localStorage.getItem('DTRMS_BY_M4RKBELLO');
+};
+
+console.log("STORE TOKEN", getTokenFromLocalStorage());
+
+Api.interceptors.request.use(
+  (config) => {
+    // Get the token from localStorage
+    const token = getTokenFromLocalStorage();
+
+    // Include the token in the request headers
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+
 
 
 export default Api;
