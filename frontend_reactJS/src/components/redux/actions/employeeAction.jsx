08aeeef191db1877f07ddddedb03b85f801a0218
m4rkbello <markbello10041999@gmail.com>
2024-04-14 14:20:16 +0800

    REDUX: add toaster to know if it's working success or failed!

--- frontend_reactJS/src/components/redux/actions/employeeAction.jsx
+++ frontend_reactJS/src/components/redux/actions/employeeAction.jsx
@@ -1,6 +1,6 @@
 import MarkBelloApi from '../../../services/Api.jsx';
-// import { toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import {
     FETCH_EMPLOYEES_REQUEST,
@@ -58,16 +58,51 @@ export const updateEmployee = (employeeId, updateEmployeeData) => async dispatch
     try {
         dispatch({ type: UPDATE_EMPLOYEE_REQUEST });
         // Perform async operation, e.g., send updated data to an API
+        document.getElementById('loading-infinity').classList.add('loading', 'loading-infinity', 'loading-lg');
         const updateEmployee = await MarkBelloApi.put(`/api/employee/${employeeId}`, updateEmployeeData);
+
         dispatch({
             type: UPDATE_EMPLOYEE_SUCCESS,
             payload: updateEmployee
         });
+
+        toast.success('Registered successfully!🤭😇🤗', {
+            position: 'top-right',
+            autoClose: 10000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            style: {
+                background: 'white',
+                color: 'green',
+                fontSize: '15px'
+            }
+        });
+
     } catch (error) {
+        console.log("ERROR SA CATCH SA UPDATE EMPLOYEE", error);
         dispatch({
             type: UPDATE_EMPLOYEE_FAILURE,
             payload: error.message
         });
+
+        // toast.error('Fill-up correctly! 🥺⚠️👽', {
+        //     position: 'top-right',
+        //     autoClose: 10000,
+        //     hideProgressBar: false,
+        //     closeOnClick: false,
+        //     pauseOnHover: false,
+        //     draggable: true,
+        //     progress: undefined,
+        //     style: {
+        //         background: 'black',
+        //         color: 'red',
+        //         fontSize: '15px'
+        //     }
+        // });
+
     }
 };
 
