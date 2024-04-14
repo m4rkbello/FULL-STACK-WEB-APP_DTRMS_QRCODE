7851988db1ee28551519bace7a8ed886435ed624
m4rkbello <markbello10041999@gmail.com>
2024-04-14 15:14:24 +0800

    REDUX: add condition to validate if it's true okay or not

--- frontend_reactJS/src/components/redux/actions/employeeAction.jsx
+++ frontend_reactJS/src/components/redux/actions/employeeAction.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import MarkBelloApi from '../../../services/Api.jsx';
+// import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -16,6 +18,10 @@ import {
     DELETE_EMPLOYEE_SUCCESS,
     DELETE_EMPLOYEE_FAILURE,
 } from '../types/employeeTypes.jsx';
+
+
+// const navigate = useNavigate();
+
 
 //MAG-FETCH UG EMPLOYEE
 export const fetchEmployees = () => async dispatch => {
@@ -59,52 +65,84 @@ export const updateEmployee = (employeeId, updateEmployeeData) => async dispatch
         dispatch({ type: UPDATE_EMPLOYEE_REQUEST });
         // Perform async operation, e.g., send updated data to an API
         document.getElementById('loading-infinity').classList.add('loading', 'loading-infinity', 'loading-lg');
-        const updateEmployee = await MarkBelloApi.put(`/api/employee/${employeeId}`, updateEmployeeData);
+        const updateEmployeeResponse = await MarkBelloApi.put(`/api/employee/${employeeId}`, updateEmployeeData);
 
-        dispatch({
-            type: UPDATE_EMPLOYEE_SUCCESS,
-            payload: updateEmployee
-        });
+        if (!updateEmployeeResponse) {
+            // Handle the case where the response is empty
+            toast.error('Fill-up correctly! 🥺⚠️👽', {
+                position: 'top-right',
+                autoClose: 10000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: false,
+                draggable: true,
+                progress: undefined,
+                style: {
+                    background: 'black',
+                    color: 'red',
+                    fontSize: '15px'
+                }
+            });
+        } else {
+            // Handle the case where the update is successful
+            toast.success('Updated Successfully!👌👌👌', {
+                position: 'top-right',
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                style: {
+                    background: 'white',
+                    color: 'green',
+                    fontSize: '15px'
+                }
+            });
 
-        toast.success('Registered successfully!🤭😇🤗', {
-            position: 'top-right',
-            autoClose: 10000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            style: {
-                background: 'white',
-                color: 'green',
-                fontSize: '15px'
-            }
-        });
+            setTimeout(() => {
+                window.location.reload();
+              }, 3000)
 
+            dispatch({
+                type: UPDATE_EMPLOYEE_SUCCESS,
+                payload: updateEmployeeResponse
+            });
+        }
     } catch (error) {
         console.log("ERROR SA CATCH SA UPDATE EMPLOYEE", error);
-        dispatch({
-            type: UPDATE_EMPLOYEE_FAILURE,
-            payload: error.message
-        });
+        // Check if the error object has a response and the status code is 500
+        if (error.response && error.response.status !== 200 || error.response && error.response.status !== 201) {
+            // Handle the case where the server returns a 500 error
+            toast.error('Something went wrong! 😛😛😛', {
+                position: 'top-right',
+                autoClose: 3000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: false,
+                draggable: true,
+                progress: undefined,
+                style: {
+                    background: 'black',
+                    color: 'red',
+                    fontSize: '15px'
+                }
+            });
 
-        // toast.error('Fill-up correctly! 🥺⚠️👽', {
-        //     position: 'top-right',
-        //     autoClose: 10000,
-        //     hideProgressBar: false,
-        //     closeOnClick: false,
-        //     pauseOnHover: false,
-        //     draggable: true,
-        //     progress: undefined,
-        //     style: {
-        //         background: 'black',
-        //         color: 'red',
-        //         fontSize: '15px'
-        //     }
-        // });
+            setTimeout(() => {
+                window.location.reload();
+              }, 3000)
 
+        } else {
+            // Handle other types of errors
+            dispatch({
+                type: UPDATE_EMPLOYEE_FAILURE,
+                payload: error.message
+            });
+        }
     }
 };
+
 
 //MAG DELETE UG EMPLOYEE
 export const deleteEMPLOYEE = employeeId => async dispatch => {
