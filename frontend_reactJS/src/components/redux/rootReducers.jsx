42dd49b16deb27eaf067236b274ab8adfa548992
m4rkbello <markbello10041999@gmail.com>
2024-04-14 19:43:14 +0800

    FRONTEND: add rootReducers for imageReducer

--- frontend_reactJS/src/components/redux/rootReducers.jsx
+++ frontend_reactJS/src/components/redux/rootReducers.jsx
@@ -2,11 +2,14 @@ import { combineReducers } from 'redux';
 import userReducer from './reducers/userReducer';
 import employeeReducer from './reducers/employeeReducer';
 import attendanceReducer from './reducers/attendanceReducer';
+import imageReducer from './reducers/imageReducer';
+
 
 const rootReducer = combineReducers({
     userState: userReducer,
     employeeState: employeeReducer,
-    attendanceState: attendanceReducer
+    attendanceState: attendanceReducer,
+    imageState: imageReducer,
 });
 
 export default rootReducer;
