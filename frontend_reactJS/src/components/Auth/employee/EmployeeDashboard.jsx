0f80561bd3147b64453aafefe48e21eac1cde437
m4rkbello <markbello10041999@gmail.com>
2024-04-19 00:29:09 +0800

    FRONTEND: add response path from public folder

--- frontend_reactJS/src/components/Auth/employee/EmployeeDashboard.jsx
+++ frontend_reactJS/src/components/Auth/employee/EmployeeDashboard.jsx
@@ -67,7 +67,7 @@ const EmployeeDashboard = (props) => {
                                                 <div className="flex items-center gap-3">
                                                     <div className="avatar">
                                                         <div className="mask mask-squircle w-12 h-12 shadow-2xl">
-                                                            <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                                                            <img src="http://127.0.0.1:8000/images/1713457582.jpg" alt="Avatar Tailwind CSS Component" />
                                                         </div>
                                                     </div>
                                                 </div>
