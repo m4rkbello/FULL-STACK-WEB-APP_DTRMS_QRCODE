514e79d3b55aa2c99f48857d119d1896d6961b54
m4rkbello <markbello10041999@gmail.com>
2024-04-21 20:54:54 +0800

    FRONTEND: add 404NotFound file for the use if the end-user access path if not authenticated by the cookies, localStorage and Session storage it will display this as the route was added a glabal "*"

---
+++ frontend_reactJS/src/components/Auth/admin/pages/404NotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const FourOFourNotFound = () => {
+  return (
+    <div>
+      <div className="flex flex-col gap-8 w-96">
+        <div className="skeleton h-64 w-full"></div>
+        <div className="skeleton h-8 w-56"></div>
+        <div className="skeleton h-8 w-full"></div>
+        <div className="skeleton h-8 w-full"></div>
+      </div>
+    </div>
+  )
+}
+
+export default FourOFourNotFound;
