c021640ff8dc61ad872bf4605d6b47ea6813970a
m4rkbello <markbello10041999@gmail.com>
2024-04-21 10:42:21 +0800

    FRONTEND: add placeholder for password

--- frontend_reactJS/src/components/Auth/admin/Login.jsx
+++ frontend_reactJS/src/components/Auth/admin/Login.jsx
@@ -73,7 +73,7 @@ const Login = ({ loginUser }) => {
               */}
                 <label className="input input-bordered flex items-center gap-2 bg-amber-100">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="black" className="w-6 h-6 opacity-100"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
-                  <input value={localPassword} onChange={(e) => setLocalPassword(e.target.value)} type="password" className="grow bg-amber-100 text-black" />
+                  <input value={localPassword} onChange={(e) => setLocalPassword(e.target.value)} type="password" className="grow bg-amber-100 text-black" placeholder="Password"  />
                 </label>
                 <label className="label">
                   <a href="#" className="label-text-alt link link-hover text-black text-xl">Forgot password?</a>
