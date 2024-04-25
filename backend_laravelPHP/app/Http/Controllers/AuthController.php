7d7f588872ab0fc6c92e9182261846d0c7a848ab
m4rkbello <markbello10041999@gmail.com>
2024-04-25 22:51:49 +0800

    BACKEND: upload and update user image with a validation of not format .jpeg, .png .gif it won't upload and it throws error toaster

--- backend_laravelPHP/app/Http/Controllers/AuthController.php
+++ backend_laravelPHP/app/Http/Controllers/AuthController.php
@@ -121,9 +121,17 @@ class AuthController extends Controller
 
     public function updateImage(Request $request, $id)
     {
-        $request->validate([
-            'user_image' => 'required|image|mimes:jpeg,png,jpg,gif|max:10240',
+        $validator = Validator::make($request->all(), [
+            'user_image' => 'required|image|mimes:jpeg,png,jpg,gif|max:10240', // Max size is 10MB lang 
         ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'status' => 400,
+                'message' => $validator->errors()->first('user_image'),
+            ]);
+        }
     
         $user = User::findOrFail($id);
     
