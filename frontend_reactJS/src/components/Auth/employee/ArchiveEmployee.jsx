824c14fb4a47c44bf9d5fa543b2bd40828ab5182
m4rkbello <markbello10041999@gmail.com>
2024-05-05 12:21:02 +0800

    FRONTEND: add breadcrumb for archive employee

--- frontend_reactJS/src/components/Auth/employee/ArchiveEmployee.jsx
+++ frontend_reactJS/src/components/Auth/employee/ArchiveEmployee.jsx
@@ -113,10 +113,36 @@ const ArchieveEmployee = (props) => {
             <button className='btn bg-amber-100'>Yes</button>
           </div>
         </dialog>
-        
+
             <div className="hero bg-amber-100 rounded-lg">
-                <div className="bg-slate-300 ">
-                    <span className="text-4xl font-black  text-black">
+                <div className="bg-black ">
+                <div class="flex flex-wrap">
+                <div>
+                <div className="text-sm breadcrumbs mb-10 bg-transparent">
+                <ul>
+                <li>
+                    <a>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path></svg>
+                    Home
+                    </a>
+                </li> 
+                <li>
+                    <a>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path></svg>
+                    Employee Archive List
+                    </a>
+                </li> 
+                <li>
+                    <span className="inline-flex gap-2 items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path></svg>
+                    Employee Personal Details
+                    </span>
+                </li>
+                </ul>
+                </div>
+                </div>
+            </div>
+                    <span className="text-4xl font-black  text-amber-100">
                         <center>
                             EMPLOPYEE ARCHIVED LIST
                         </center>
