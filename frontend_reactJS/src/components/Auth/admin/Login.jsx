b6878f513f61923ed098f7e9335167be24b8a233
m4rkbello <markbello10041999@gmail.com>
2024-05-05 09:58:53 +0800

    FRONTEND: login remove textContent

--- frontend_reactJS/src/components/Auth/admin/Login.jsx
+++ frontend_reactJS/src/components/Auth/admin/Login.jsx
@@ -45,8 +45,8 @@ const Login = ({ loginUser }) => {
       <div className="hero min-h-screen bg-transparent shadow-md">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold text-black">Login now!</h1>
-            <p className="py-6 text-black">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <h1 className="text-5xl font-bold text-black">.</h1>
+            <p className="py-6 text-black"></p>
           </div>
           <div className="card shrink-0 w-full max-w-sm shadow-md bg-gradient-to-r from-amber-100 via-black to-black/50 to-black/50">
             <form className="card-body">
