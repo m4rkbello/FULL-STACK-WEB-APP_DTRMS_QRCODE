6277a895f19c603f05b32be5767969c7e5f5f809
m4rkbello <markbello10041999@gmail.com>
2024-05-05 12:20:08 +0800

    FRONTEND: add department

--- frontend_reactJS/src/components/Auth/admin/department/Department.jsx
+++ frontend_reactJS/src/components/Auth/admin/department/Department.jsx
@@ -2,18 +2,46 @@ import React from 'react'
 
 const Department = () => {
   return (
-    <div><h1>
-    <div className="avatar online">
-    <div className="w-24 rounded-full">
-      <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+    <div className='bg-base-200'>
+      <div className='bg-base-300'>
+        <div className="overflow-x-auto ">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Job</th>
+                <th>Favorite Color</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              <tr>
+                <th>1</th>
+                <td>Cy Ganderton</td>
+                <td>Quality Control Specialist</td>
+                <td>Blue</td>
+              </tr>
+              {/* row 2 */}
+              <tr className="hover">
+                <th>2</th>
+                <td>Hart Hagerty</td>
+                <td>Desktop Support Technician</td>
+                <td>Purple</td>
+              </tr>
+              {/* row 3 */}
+              <tr>
+                <th>3</th>
+                <td>Brice Swyre</td>
+                <td>Tax Accountant</td>
+                <td>Red</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
-  </div>
-  <div className="avatar offline">
-    <div className="w-24 rounded-full">
-      <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-    </div>
-  </div>
-    </h1></div>
   )
 }
 
