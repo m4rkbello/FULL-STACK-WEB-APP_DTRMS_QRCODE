ca3d60e4657f6f10efa38a8defa7f8160fcd93df
m4rkbello <markbello10041999@gmail.com>
2024-05-06 22:10:47 +0800

    tugpa

--- backend_laravelPHP/routes/api.php
+++ backend_laravelPHP/routes/api.php
@@ -32,11 +32,12 @@ Route::post('/employee/image/{id}', [EmployeeController::class, 'uploadAndUpdate
 // ATTENDANCE
 
 // MIDDLEWARE FOR FRONTEND-BACKEND 
-Route::post('/employee-registration', [EmployeeController::class, 'store']);
-Route::put('/employee/deactivated/{id}', [EmployeeController::class, 'deactivate']);
 
 
 Route::middleware('auth:sanctum')->group(function() {
+    
+    Route::post('/employee-registration', [EmployeeController::class, 'store']);
+    Route::put('/employee/deactivated/{id}', [EmployeeController::class, 'deactivate']);
     Route::put('/user/{id}', [AuthController::class, 'update']);
     Route::put('/employee/{id}', [EmployeeController::class, 'update']);
     Route::post('/employee/search/', [EmployeeController::class, 'search']);
