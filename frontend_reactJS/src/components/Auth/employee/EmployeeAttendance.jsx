85b0aa5a7c113d839700bc4ade31167f4f3bc9f2
m4rkbello <markbello10041999@gmail.com>
2024-05-10 22:08:18 +0800

    FRONTEND: full w x h

--- frontend_reactJS/src/components/Auth/employee/EmployeeAttendance.jsx
+++ frontend_reactJS/src/components/Auth/employee/EmployeeAttendance.jsx
@@ -3,7 +3,7 @@ import EmployeeChart from './EmployeeChart';
 
 const EmployeeAttendance = () => {
   return (
-    <div>
+    <div style={{ width: '100%', height: '100vh' }}>
       <EmployeeChart />
     </div>
   );
