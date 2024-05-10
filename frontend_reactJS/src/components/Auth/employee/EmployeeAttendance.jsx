aad87f902de77b1b9545c8a990ce47b30d9d47ae
m4rkbello <markbello10041999@gmail.com>
2024-05-10 22:03:31 +0800

    FRONTEND: wrappping the  chart to the component as parent component

--- frontend_reactJS/src/components/Auth/employee/EmployeeAttendance.jsx
+++ frontend_reactJS/src/components/Auth/employee/EmployeeAttendance.jsx
@@ -1,20 +1,12 @@
-import React from 'react'
+import React from 'react';
+import EmployeeChart from './EmployeeChart';
 
 const EmployeeAttendance = () => {
   return (
     <div>
-    <div className="flex flex-col w-full lg:flex-row">
-  <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div> 
-  <div className="divider lg:divider-horizontal">OR</div> 
-  <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div>
-</div>
-<div className="flex flex-col w-full border-opacity-50">
-  <div className="grid h-20 card bg-base-300 rounded-box place-items-center">content</div>
-  <div className="divider">OR</div>
-  <div className="grid h-20 card bg-base-300 rounded-box place-items-center">content</div>
-</div>
+      <EmployeeChart />
     </div>
-  )
-}
+  );
+};
 
-export default EmployeeAttendance
+export default EmployeeAttendance;
