022467140d357f4821dbab7b70eff45cae5e5197
m4rkbello <markbello10041999@gmail.com>
2024-05-10 22:04:53 +0800

    REDUX: add to the rootReducer as same as redux

--- frontend_reactJS/src/components/redux/rootReducers.jsx
+++ frontend_reactJS/src/components/redux/rootReducers.jsx
@@ -3,13 +3,14 @@ import userReducer from './reducers/userReducer';
 import employeeReducer from './reducers/employeeReducer';
 import attendanceReducer from './reducers/attendanceReducer';
 import imageReducer from './reducers/imageReducer';
-
+import chartReducer from '../../services/chart/chartReducer';
 
 const rootReducer = combineReducers({
-    userState: userReducer,
-    employeeState: employeeReducer,
-    attendanceState: attendanceReducer,
-    imageState: imageReducer,
+  userState: userReducer,
+  employeeState: employeeReducer,
+  attendanceState: attendanceReducer,
+  imageState: imageReducer,
+  chartState: chartReducer,
 });
 
 export default rootReducer;
