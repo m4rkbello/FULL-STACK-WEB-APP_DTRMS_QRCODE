608c5725de9dfbacc450532d5790a84175cc14c2
m4rkbello <markbello10041999@gmail.com>
2024-05-12 18:18:13 +0800

    FRONTEND: add departmentReducer as departmentState to the rootReducer

--- frontend_reactJS/src/components/redux/rootReducers.jsx
+++ frontend_reactJS/src/components/redux/rootReducers.jsx
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import userReducer from './reducers/userReducer';
 import employeeReducer from './reducers/employeeReducer';
+import departmentReducer from './reducers/departmentReducer';
 import attendanceReducer from './reducers/attendanceReducer';
 import imageReducer from './reducers/imageReducer';
 import chartReducer from '../../services/chart/chartReducer';
@@ -8,9 +9,13 @@ import chartReducer from '../../services/chart/chartReducer';
 const rootReducer = combineReducers({
   userState: userReducer,
   employeeState: employeeReducer,
+  departmentState: departmentReducer,
   attendanceState: attendanceReducer,
   imageState: imageReducer,
   chartState: chartReducer,
 });
+
+
+
 
 export default rootReducer;
