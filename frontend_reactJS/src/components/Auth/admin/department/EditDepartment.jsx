093811ddfca82dfeb3f6c8087e29279f5411d842
m4rkbello <markbello10041999@gmail.com>
2024-05-16 22:21:00 +0800

    FRONTEND: edit details

--- frontend_reactJS/src/components/Auth/admin/department/EditDepartment.jsx
+++ frontend_reactJS/src/components/Auth/admin/department/EditDepartment.jsx
@@ -4,7 +4,7 @@
 /* eslint-disable react-refresh/only-export-components */
 import { connect } from 'react-redux';
 import { useEffect, useState } from 'react';
-import { useParams, useNavigate,Link } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 //DISPATCH-ACTION REDUX-CORE
 import { fetchDepartments, updateDepartment } from '../../../redux/actions/departmentAction';
 //ICONS
@@ -17,11 +17,11 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 const EditDepartment = (props) => {
-    
+
     const { id } = useParams();
     const [isLoading, setIsLoading] = useState(false);
     const [isModalOpen, setIsModalOpen] = useState(false);
-    
+
     console.log("DATA SA FETCH DEPARTMENT", props);
     console.log("SELECTED ID SA USE PARAMS", id);
 
@@ -69,9 +69,9 @@ const EditDepartment = (props) => {
             window.alert("ERROR");
         };
     }
-    
+
     const departmentCollectionArrays = props.departmentData?.departments?.data?.department;
-    
+
     function departmentsDetails(departmentCollectionArrays, id) {
         let item = [];
 
@@ -84,157 +84,204 @@ const EditDepartment = (props) => {
         }
         return item;
     }
-    
+
     const departments = departmentsDetails(departmentCollectionArrays, id);
     // console.log("AYAW KOL", departments);
 
     if (props.loading) {
         return <div>
-        {/**
+            {/**
         <span className="loading loading-ball loading-xs"></span>
         <span className="loading loading-ball loading-sm"></span>
         <span className="loading loading-ball loading-md"></span>
         <span className="loading loading-ball loading-lg"></span>
         */}
-        <progress className="progress w-56"></progress>
+            <progress className="progress w-56"></progress>
         </div>;
-      }
+    }
 
     return (
         <div>
-        <dialog id="addEmployeeModal" className="modal ">
-        <div className="modal-box w-11/12 max-w-5xl bg-amber-100">
-            <h3 className="font-bold text-3xl text-black">ADD EMPLOYEE</h3>
-            <div className="modal-action">
-                <form method="dialog">
-                    <div className="grid grid-cols-3 gap-6">
 
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text text-black text-2xl">Fullname</span>
-                            </label>
-                            <input
-                              
-                                name="employee_fullname" //key para sa form data
-                                type="text"
-                                placeholder="Enter Fullname"
-                                className="input input-bordered shadow-2xl text-2xl  text-amber-100"
-                                // onChange={(e) => setFormDataEmployeeAddEmployee(prevState => ({ ...prevState, employee_fullname: e.target.value }))}
-                                // value={formDataAddEmployee.employee_fullname}
-                                style={{ backgroundColor: 'black' }}
-                            />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text text-black text-2xl">Email</span>
-                            </label>
-                            <input
-                                key=""
-                                name="employee_fullname" //key para sa form data
-                                type="text"
-                                placeholder="Enter email"
-                                className="input input-bordered shadow-2xl text-2xl  text-amber-100"
-                                // onChange={(e) => setFormDataEmployeeAddEmployee(prevState => ({...prevState, employee_email: e.target.value}))}
-                                // value={formDataAddEmployee.employee_email}    
-                                style={{ backgroundColor: 'black' }}
-                            />
-                        </div>
-                        <div className="form-control">
-                        <label className="label">
-                            <span className="label-text text-black text-2xl">Department</span>
-                        </label>
-                        <input
-                            key=""
-                            name="employee_department" //key para sa form data
-                            type="text"
-                            placeholder="Enter email"
-                            className="input input-bordered shadow-2xl text-2xl  text-amber-100"
-                            // onChange={(e) => setFormDataEmployeeAddEmployee(prevState => ({...prevState, employee_department: e.target.value}))}
-                            // value={formDataAddEmployee.employee_department}    
-                            style={{ backgroundColor: 'black' }}
-                        />
-                    </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text text-black text-2xl">Contact No.</span>
-                            </label>
-                            <input
-        
-                                name="employee_contact_no" //key para sa form data
-                                type="text"
-                                placeholder="Enter contact no."
-                                className="input input-bordered shadow-2xl text-2xl  text-amber-100"
-                                // onChange={(e) => setFormDataEmployeeAddEmployee(prevState =>({...prevState, employee_contact_no: e.target.value}))}
-                                // value={formDataAddEmployee.employee_contact_no}
-                                style={{ backgroundColor: 'black' }}
-                            />
-                        </div>
-                    </div>
-                    <div className="grid grid-cols-3 gap-6">
+            <dialog id="editEmployeeDetails" className="modal">
+                <div className=" modal-box w-11/12 max-w-5xl bg-amber-100">
+                    <h3 className="font-bold text-3xl text-black">EDIT EMPLOYEE DETAILS</h3>
 
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text text-black text-2xl">Position</span>
-                            </label>
-                            <input
-                            
-                                name="employee_position" //key para sa form data
-                                type="text"
-                                placeholder="Enter Position"
-                                className="input input-bordered shadow-2xl text-2xl  text-amber-100"
-                                // onChange={(e) => setFormDataEmployeeAddEmployee(prevState => ({...prevState, employee_position: e.target.value}))}
-                                // value={formDataAddEmployee.employee_position}
-                                style={{ backgroundColor: 'black' }}
-                            />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text text-black text-2xl">Role</span>
-                            </label>
-                            <input
-                          
-                                name="employee_role" //key para sa form data
-                                type="text"
-                                placeholder="Enter role"
-                                className="input input-bordered shadow-2xl text-2xl  text-amber-100"
-                               
-                                
-                                // value={formDataAddEmployee.employee_role}
-                                style={{ backgroundColor: 'black' }}
-                            />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text text-black text-2xl">Status</span>
-                            </label>
-                            <input
-                              
-                                name="employee_status" //key para sa form data
-                                type="text"
-                                placeholder="Enter status(Active/Inactive)"
-                                className="input input-bordered shadow-2xl text-2xl  text-amber-100"
-                                // onChange={(e) => setFormDataEmployeeAddEmployee(prevState => ({...prevState, employee_status: e.target.value}))}
-                                // value={formDataAddEmployee.employee_status}
-                                style={{ backgroundColor: 'black' }}
-                            />
-                        </div>
-                    </div>
+                    <div className="modal-action">
+                        <form method="dialog" onSubmit="">
+                            <div className="grid grid-cols-3 gap-6">
 
-                    <br />
-                    <div className='flex '>
-                        <div className='flex-initial pr-2'>
-                            <button type="submit" className="btn bg-black text-amber-100">Add</button>
-                        </div>
-                        <div className='flex-initial'>
-                            <button className="btn bg-black text-amber-100">Close</button>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text text-black text-2xl">Fullname</span>
+                                    </label>
+
+                                    <input
+
+                                        name="employee_fullname" //key para sa form data
+
+                                        type="text"
+                                        placeholder="text"
+                                        className="input input-bordered shadow-2xl text-2xl  text-amber-100"
+
+                                        style={{ backgroundColor: 'black' }}
+                                    />
+
+
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text text-black text-2xl">Email</span>
+                                    </label>
+
+                                    <input
+
+                                        name="employee_email" //key para sa formData
+
+                                        type="text"
+                                        placeholder="email"
+                                        className="input input-bordered shadow-2xl text-2xl  text-amber-100"
+
+                                        style={{ backgroundColor: 'black' }}
+                                    />
+
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text text-black text-2xl">Contact No.</span>
+                                    </label>
+
+                                    <input
+
+                                        type="text"
+                                        name="employee_contact_no"
+
+                                        placeholder="contact number"
+                                        className="input input-bordered shadow-2xl text-2xl  text-amber-100"
+
+                                        style={{ backgroundColor: 'black' }}
+                                    />
+
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text text-black text-2xl">Role</span>
+                                    </label>
+
+                                    <input
+
+                                        type="text"
+                                        name="employee_role"
+
+                                        placeholder="Role"
+                                        className="input input-bordered shadow-2xl text-2xl  text-amber-100"
+
+                                        style={{ backgroundColor: 'black' }}
+                                    />
+
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text text-black text-2xl">Position</span>
+                                    </label>
+
+                                    <input
+
+                                        type="text"
+                                        name="employee_position"
+
+                                        placeholder="Position"
+                                        className="input input-bordered shadow-2xl text-2xl  text-amber-100"
+
+                                        style={{ backgroundColor: 'black' }}
+                                    />
+
+
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text text-black text-2xl">Department</span>
+                                    </label>
+
+                                    <input
+
+                                        type="text"
+                                        name="employee_department"
+
+                                        placeholder="Enter a department"
+                                        className="input input-bordered shadow-2xl text-2xl text-amber-100"
+
+                                        style={{ backgroundColor: 'black' }}
+                                    />
+
+                                </div>
+                                <center>
+                                </center>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text text-black text-2xl">Status</span>
+                                    </label>
+
+                                    <select
+
+                                        name="employee_status"
+                                        className="select shadow-2xl text-2xl w-full max-w-xs"
+                                        style={{ backgroundColor: 'black', color: "#fef3c6" }}
+
+                                    >
+                                        <option value="1">Active</option>
+                                        <option value="0">Inactive</option>
+                                    </select>
+
+                                </div>
+
+
+                            </div>
+                            <br />
+                            <div className="flex ...">
+                                <div>
+                                    <button type="submit" className="btn bg-transparent" style={{ fontSize: "40px", color: "black", border: "none" }} >
+                                        <FaSave style={{ fontSize: "25px", color: "black", marginRight: "5px" }} />
+                                    </button>
+                                </div>
+
+                                <div>
+                                    <button onClick={handleCloseModal} className="btn bg-transparent" style={{ fontSize: "40px", color: "black", border: "none" }} >
+                                        <IoMdCloseCircle style={{ fontSize: "25px", color: "black", marginRight: "5px" }} />
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+
+
+                    </div>
+                    <center>
+                        <span id="loading-infinity" className={`loading loading-infinity loading-lg ${isLoading ? 'block' : 'hidden'} spinner-blue`}></span>
+                    </center>
+                </div>
+            </dialog>
+
+            <div className="hero min-h-screen bg-amber-100 rounded-t-lg">
+
+                <button style={{ marginRight: "93%", marginBottom: "65%" }} >
+                    <Link to="/employee/dashboard">
+                        <FaLongArrowAltLeft style={{ fontSize: "50px", color: "black", marginRight: "90%", marginBottom: "65%" }} />
+                    </Link>
+                </button>
+
+                <div className="hero-content flex flex-col items-center">
+                    <div className="hero-content flex-col lg:flex-row py-0 px-0">
+
+                        <div className="flex-1">
+                            <button onClick={handleOpenModal}>
+                                <FaUserEdit onClick={() => document.getElementById('editEmployeeDetails').showModal()} style={{ fontSize: "40px", color: "black", marginLeft: "-65%", marginBottom: "-100" }} />
+                            </button>
                         </div>
                     </div>
-                </form>
+                </div>
             </div>
-        </div>
-    </dialog>
 
-    
+
         </div>
     )
 }
@@ -244,15 +291,13 @@ const mapStateToProps = (state) => {
         departmentData: state.departmentState,
         loading: state.departmentState.loading,
     }
-};
+}
 
 const mapDispatchToProps = (dispatch) => {
     return {
         fetchDepartments: () => dispatch(fetchDepartments()),
-        updateDepartment: (deptartmentId, updateDepartmentData) => dispatch(updateDepartment(deptartmentId, updateDepartmentData)),
+        updateDepartment: (departmentId, updateDepartmentData) => dispatch(updateDepartment(departmentId, updateDepartmentData)),
     }
-};
-
-
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(EditDepartment);
