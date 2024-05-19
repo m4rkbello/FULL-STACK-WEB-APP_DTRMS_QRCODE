4d65b7738e337d719c71033c953d5ce5d1496e81
m4rkbello <markbello10041999@gmail.com>
2024-05-19 19:29:22 +0800

    FULLSTACK: assorted

--- frontend_reactJS/src/components/Auth/admin/department/Department.jsx
+++ frontend_reactJS/src/components/Auth/admin/department/Department.jsx
@@ -8,18 +8,15 @@ import { HiStatusOnline } from "react-icons/hi";
 import { MdOutlineNoAccounts } from "react-icons/md";
 import { RiAccountPinCircleFill } from "react-icons/ri";
 
-//DEPARTMENT ACTIONS/DISPATCH
+// DEPARTMENT ACTIONS/DISPATCH
 import { fetchDepartments } from '../../../redux/actions/departmentAction';
 
 const Department = (props) => {
-  // console.log("DATA SA FETCH DEPARTMENT", props && props);
-  
   useEffect(() => {
     props.fetchDepartments();
   },[])
 
   const getAllDepartmentCollectionArrays = props?.departmentData?.departments?.data?.department;
-  console.log("DATA SA PROPS DRILLING", getAllDepartmentCollectionArrays);
 
   const getAllDepartments = (getAllDepartmentCollectionArrays) => {
     let items = [];
@@ -32,73 +29,69 @@ const Department = (props) => {
   }
   
   const departmentArrays = getAllDepartments(getAllDepartmentCollectionArrays);
-  // console.log("DATA NA GIKAN SA ARROW FUNCTION FOREACH", departmentArrays);
 
   if (props.loading) {
-    return <div>
-    <span className="loading loading-ball loading-xs"></span>
-    <span className="loading loading-ball loading-sm"></span>
-    <span className="loading loading-ball loading-md"></span>
-    <span className="loading loading-ball loading-lg"></span>
-    </div>;
+    return (
+      <div>
+        <span className="loading loading-ball loading-xs"></span>
+        <span className="loading loading-ball loading-sm"></span>
+        <span className="loading loading-ball loading-md"></span>
+        <span className="loading loading-ball loading-lg"></span>
+      </div>
+    );
   }
 
   return (
-    <div className='bg-base-200 h-full w-full'>
-      <div className='bg-base-300 h-full w-full'>
-        <div className="overflow-x-auto">
-        {Array.isArray(departmentArrays) && departmentArrays.length > 0 ? (
-
-          <table className="table w-full h-full">
-              <thead className='bg-amber-100 pr-3 pl-3 pb-3 pt-3'>
+    <div className='bg-base-200 w-full'>
+      <div className='bg-base-300  h-full'>
+        <div className="overflow-auto max-h-96"> {/* Add this div */}
+          {Array.isArray(departmentArrays) && departmentArrays.length > 0 ? (
+            <table className="table w-full">
+              <thead className='bg-amber-100'>
                 <tr>
                   <th className='text-1xl text-black'>DEPARTMENT NAME</th>
                   <th className='text-1xl text-black'>DEPARTMENT DESCRIPTION</th>
                   <th className='text-1xl text-black'>DEPARTMENT STATUS</th>
                   <th className='text-1xl text-black'>ACTION</th>
                   <th className='text-1xl text-black'>
-                  <Link to="/department/add" className="text-black">
-                  <IoIosPersonAdd
-                  style={{ height: "50px", width: "50px", color: "black" }}
-                  />
-                  </Link>
+                    <Link to="/department/add" className="text-black">
+                      <IoIosPersonAdd style={{ height: "50px", width: "50px", color: "black" }} />
+                    </Link>
                   </th>
                 </tr>
               </thead>
               <tbody>
-              {departmentArrays && departmentArrays.map((item, index) => (
-                <tr key={index}>
-                  <td>{item.dept_name}</td>
-                  <td>{item.dept_description}</td>
-                  <td>{item.dept_status_id}</td>
-                  <td>
-                  <div className="flex">
-                  <div className="flex-none mr-3">
-                      <Link to={`/department/edit/${item.id}`} className="text-black">
-                          <FaEye style={{ fontSize: "20px", color: "#fef3c6", padding: "0%" }} />
-                      </Link>
-
-                  </div>
-                  <div className="flex-none mr-3">
-                      <MdAutoDelete
-                      onClick={() => {
+                {departmentArrays && departmentArrays.map((item, index) => (
+                  <tr key={index}>
+                    <td>{item.dept_name}</td>
+                    <td>{item.dept_description}</td>
+                    <td>{item.dept_status_id}</td>
+                    <td>
+                      <div className="flex">
+                        <div className="flex-none mr-3">
+                          <Link to={`/department/edit/${item.id}`} className="text-black">
+                            <FaEye style={{ fontSize: "20px", color: "#fef3c6", padding: "0%" }} />
+                          </Link>
+                        </div>
+                        <div className="flex-none mr-3">
+                          <MdAutoDelete
+                            onClick={() => {
                               setDeactivateEmployeeId(item.id); 
                               document.getElementById('removeEmployee').showModal()
-                          }}
-                       style={{ fontSize: "20px", color: "#fef3c6" }} />
-                  </div>
-              </div>
-                  </td>
-                </tr>
-              ))}
-              
+                            }}
+                            style={{ fontSize: "20px", color: "#fef3c6" }}
+                          />
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
-        ) : (
-          <h1>NO DATA</h1>
-        )
-       } 
-        </div>  
+          ) : (
+            <h1>NO DATA</h1>
+          )}
+        </div>
       </div>
     </div>
   )
