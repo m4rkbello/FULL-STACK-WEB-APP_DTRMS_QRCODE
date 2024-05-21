1fb7f052d70476da5b607349511a450b21ffeb51
m4rkbello <markbello10041999@gmail.com>
2024-05-22 01:24:52 +0800

    FRONTEND: new color way

--- frontend_reactJS/src/components/Auth/admin/Register.jsx
+++ frontend_reactJS/src/components/Auth/admin/Register.jsx
@@ -67,52 +67,52 @@ const Register = ({registerUser}) => {
         
           <div className="card shrink-0 w-full max-w-sm shadow-md bg-gradient-to-r from-amber-100 via-black to-black/50 to-black/50">
             */}
-          <div className="card shrink-0 w-full max-w-sm shadow-md glass">
+          <div className="card shrink-0 w-full max-w-sm shadow-md bg-black">
 
                     <form className="card-body">
 
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text text-xl text-black">Firstname</span>
+                                <span className="label-text text-xl text-white">Firstname</span>
                             </label>
-                            <input type="text" value={localFirstName} onChange={(e) => setLocalFirstName(e.target.value)} placeholder="name" className="input input-bordered grow bg-amber-100 text-blac" required />
+                            <input type="text" value={localFirstName} onChange={(e) => setLocalFirstName(e.target.value)} placeholder="name" className="input input-bordered grow glass text-lime-400" required />
                         </div>
                         <div className="form-control">
                         <label className="label">
-                            <span className="label-text text-xl text-black">Lastname</span>
+                            <span className="label-text text-xl text-white">Lastname</span>
                         </label>
-                        <input type="text" value={localLastName} onChange={(e) => setLocalLastName(e.target.value)} placeholder="name" className="input input-bordered grow bg-amber-100 text-blac" required />
+                        <input type="text" value={localLastName} onChange={(e) => setLocalLastName(e.target.value)} placeholder="name" className="input input-bordered grow glass text-lime-400" required />
                     </div>
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text text-xl text-black">Email</span>
+                                <span className="label-text text-xl text-white">Email</span>
                             </label>
-                            <input type="email" value={localEmail} onChange={(e) => setLocalEmail(e.target.value)} placeholder="email" className="input input-bordered grow bg-amber-100 text-blac" required />
+                            <input type="email" value={localEmail} onChange={(e) => setLocalEmail(e.target.value)} placeholder="email" className="input input-bordered grow glass text-lime-400" required />
                         </div>
                         <div className="form-control">
                         <label className="label">
-                            <span className="label-text text-xl text-black">Contact No.</span>
+                            <span className="label-text text-xl text-white">Contact No.</span>
                         </label>
-                        <input type="text" value={localContactNo} onChange={(e) => setLocalContactNo(e.target.value)} placeholder="contact no." className="input input-bordered grow bg-amber-100 text-blac" required />
+                        <input type="text" value={localContactNo} onChange={(e) => setLocalContactNo(e.target.value)} placeholder="contact no." className="input input-bordered grow glass text-lime-400" required />
                     </div>
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text text-xl text-black">Password</span>
+                                <span className="label-text text-xl text-white">Password</span>
                             </label>
                           
-                            <input type="password" value={localPassword} onChange={(e) => setLocalPassword(e.target.value)} placeholder="password" className="input input-bordered grow bg-amber-100 text-blacK" required />
+                            <input type="password" value={localPassword} onChange={(e) => setLocalPassword(e.target.value)} placeholder="password" className="input input-bordered grow glass text-lime-400" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text text-xl text-black">Confirm Password</span>
+                                <span className="label-text text-xl text-white">Confirm Password</span>
                             </label>
-                            <input type="password" value={localConfirmPassword} onChange={(e) => setLocalConfirmPassword(e.target.value)} placeholder="confirm password" className="input input-bordered grow bg-amber-100 text-blac" required />
+                            <input type="password" value={localConfirmPassword} onChange={(e) => setLocalConfirmPassword(e.target.value)} placeholder="confirm password" className="input input-bordered grow glass text-lime-400" required />
                             <label className="label">
-                                <Link to="/resetpassword" className="label-text text-xl text-black-alt link link-hover">Forgot password?</Link>
+                                <Link to="/resetpassword" className="label-text text-xl text-white-alt link link-hover">Forgot password?</Link>
                             </label>
                         </div>
                         <div className="form-control mt-6">
-                            <button onClick={handleRegisterUserRequestAndResponse} className="btn bg-gradient-to-r from-black to-black-100 hover:from-black hover:to-amber-100 text-amber-100 hover:text-black  text-2xl">Register</button>
+                        <button className="btn bg-gradient-to-r from-black to-black-100 hover:from-black hover:to-lime-400 text-lime-400 hover:text-black text-2xl">Register</button>
                         </div>
                         <center>
                         <span id="loading-infinity" className={`loading loading-infinity loading-lg ${isLoading ? 'block' : 'hidden'} spinner-blue`}></span>
