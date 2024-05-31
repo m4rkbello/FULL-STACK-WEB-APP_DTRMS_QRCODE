767b05b4045d8c2c354598b44339341a8afbe831
m4rkbello <markbello10041999@gmail.com>
2024-06-01 01:31:56 +0800

    tug

--- frontend_reactJS/tailwind.config.js
+++ frontend_reactJS/tailwind.config.js
@@ -8,4 +8,21 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#a3e636",
+          "secondary": "#f6d860",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#262626", // Default background color
+          "info": "#3abff8",
+          "success": "#36d399",
+          "warning": "#fbbd23",
+          "error": "#f87272",
+        },
+      },
+    ],
+  },
 }
