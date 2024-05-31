8be5a94f869a64a08df6dcb20c6a94c9a253d3d2
m4rkbello <markbello10041999@gmail.com>
2024-05-31 23:43:19 +0800

    FRONTEND: add something

--- frontend_reactJS/src/components/Auth/admin/user/UserChangePassword.jsx
+++ frontend_reactJS/src/components/Auth/admin/user/UserChangePassword.jsx
@@ -109,13 +109,19 @@ const UserDetails = (props) => {
 
   //para sa loading request if loading ang redux-reducer niya is is user_request
   if (props.loading) {
-    return <div>
-      <span className="bg-lime-400 loading loading-ball loading-xs"></span>
-      <span className="bg-lime-400 loading loading-ball loading-sm"></span>
-      <span className="bg-lime-400 loading loading-ball loading-md"></span>
-      <span className="bg-lime-400 loading loading-ball loading-lg"></span>
-    </div>;
-  }
+    return (
+      <div className="flex flex-col gap-4 w-52">
+    <div className="flex gap-4 items-center">
+      <div className="skeleton w-16 h-16 rounded-full shrink-0"></div>
+      <div className="flex flex-col gap-4">
+        <div className="skeleton h-4 w-20"></div>
+        <div className="skeleton h-4 w-28"></div>
+      </div>
+    </div>
+    <div className="skeleton h-32 w-full"></div>
+  </div>
+    )
+  };
 
   return (
 
