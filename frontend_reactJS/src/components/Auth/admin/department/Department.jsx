1d7f3e39be9b04770b618baac19c5d33ddf2dd34
m4rkbello <markbello10041999@gmail.com>
2024-06-10 22:53:12 +0800

    maTUG NATA

--- frontend_reactJS/src/components/Auth/admin/department/Department.jsx
+++ frontend_reactJS/src/components/Auth/admin/department/Department.jsx
@@ -177,18 +177,14 @@ const Department = (props) => {
             </tbody>
           </table>
         ) : (
-          <div className="hero max-h-fit bg-black rounded-t-lg rounded-b-lg rounded-l-lg rounded-r-lg">
-            <div className="card lg:card-side bg-base-100 shadow-xl">
-              <figure><img src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure>
-              <div className="card-body">
-                <h2 className="card-title">New album is released!</h2>
-                <p>Click the button to listen on Spotiwhy app.</p>
-                <div className="card-actions justify-end">
-                  <button className="btn btn-primary">Listen</button>
-                </div>
-              </div>
-            </div>
+          <div className="mockup-browser border bg-base-300 border-t-4 border-lime-400">
+          <div className="mockup-browser-toolbar">
+            <div className="input text-lime-400">https://markbello.com</div>
           </div>
+          <div className="flex justify-center px-4 py-16 bg-base-200">
+          <span className='text-lg'>AYAW PANGITAA ANG WALA!</span>
+          </div>
+        </div>
 
         )}
       </div>
