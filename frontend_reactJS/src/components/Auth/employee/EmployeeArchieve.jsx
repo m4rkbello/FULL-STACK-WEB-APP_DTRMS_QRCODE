240e8ccc7d52e17a654c740673563c07c2fb5d35
m4rkbello <markbello10041999@gmail.com>
2024-06-23 17:51:43 +0800

    FRONTEND

--- frontend_reactJS/src/components/Auth/employee/EmployeeArchieve.jsx
+++ frontend_reactJS/src/components/Auth/employee/EmployeeArchieve.jsx
@@ -254,7 +254,20 @@ const ArchieveEmployee = (props) => {
                             </table>
                             </div>
                         ) : (
-                            <h1>NO DATA</h1>
+                            <div className="mockup-browser  border border-t-4 pb-10 pt-10">
+                            <div className="mockup-browser-toolbar">
+                              <div className="input text-black-400">https://markbello.com</div>
+                            </div>
+                            <div className="flex justify-center px-4 py-16 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
+                              <span
+                                style={{ fontSize: '50px', fontWeightL: 'Bolder' }}
+                              >
+                                <b>
+                                  AYAW NA PANGITAA ANG WALA!
+                                </b>
+                              </span>
+                            </div>
+                          </div>
                         )}
 
                     </div>
