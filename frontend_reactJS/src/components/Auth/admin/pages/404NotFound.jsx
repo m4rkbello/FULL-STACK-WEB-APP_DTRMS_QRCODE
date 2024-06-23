6cfbf072aa9a821afce00dea2a8289a9b8ec1571
m4rkbello <markbello10041999@gmail.com>
2024-06-23 12:18:18 +0800

    add

--- frontend_reactJS/src/components/Auth/admin/pages/404NotFound.jsx
+++ frontend_reactJS/src/components/Auth/admin/pages/404NotFound.jsx
@@ -3,13 +3,19 @@ import React from 'react';
 const FourOFourNotFound = () => {
   return (
     <div className="mockup-browser  border border-t-4 ">
-    <div className="mockup-browser-toolbar">
-      <div className="input text-black-400">https://markbello.com</div>
+      <div className="mockup-browser-toolbar">
+        <div className="input text-black-400">https://markbello.com</div>
+      </div>
+      <div className="flex justify-center px-4 py-16 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
+        <span
+          style={{ fontSize: '50px', fontWeightL: 'Bolder' }}
+        >
+          <b>
+            AYAW NA PANGITAA ANG WALA!
+          </b>
+        </span>
+      </div>
     </div>
-    <div className="flex justify-center px-4 py-16 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
-    <span className='text-xl'>AYAW PANGITAA ANG WALA!</span>
-    </div>
-  </div>
   )
 }
 
