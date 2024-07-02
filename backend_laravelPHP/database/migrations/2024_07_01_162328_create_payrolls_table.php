075ac803e6ca20062bcc243b054ba95edbc41feb
m4rkbello <markbello10041999@gmail.com>
2024-07-02 23:36:16 +0800

    ADD

---
+++ backend_laravelPHP/database/migrations/2024_07_01_162328_create_payrolls_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payrolls', function (Blueprint $table) {
+            $table->id();
+            $table->integer('payroll_data');
+            $table->string('payroll_details');
+            $table->string('payroll_description');
+            $table->string('payroll_department');
+            $table->integer('payroll_status_id')->nullable();
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payrolls');
+    }
+};
