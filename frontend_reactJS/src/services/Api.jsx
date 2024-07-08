132e63be1eead48f78fbc47f697e4e964cdcace3
m4rkbello <markbello10041999@gmail.com>
2024-07-08 22:42:01 +0800

    FRONTEND: API

--- frontend_reactJS/src/services/Api.jsx
+++ frontend_reactJS/src/services/Api.jsx
@@ -1,40 +1,8 @@
-// import axios from 'axios';
-
-// const Api = axios.create({
-//   baseURL: import.meta.env.VITE_API_BASE_URL,
-//   withCredentials: true
-// });
-
-// const getTokenFromSessionStorage = () => {
-//   return sessionStorage.getItem('DTRMS_BY_M4RKBELLO');
-// };
-
-// const setAuthorizationToken = (config) => {
-//   const token = getTokenFromSessionStorage();
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// };
-
-// Api.interceptors.request.use(
-//   setAuthorizationToken,
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default Api;
-
-
 import axios from 'axios';
 
 const Api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json'
-  }
+  withCredentials: true
 });
 
 const getTokenFromSessionStorage = () => {
@@ -50,16 +18,47 @@ const setAuthorizationToken = (config) => {
 };
 
 Api.interceptors.request.use(
-  (config) => {
-    // Ensure Content-Type is set for POST requests
-    if (config.method === 'post') {
-      config.headers['Content-Type'] = 'application/json';
-    }
-    return setAuthorizationToken(config);
-  },
+  setAuthorizationToken,
   (error) => {
     return Promise.reject(error);
   }
 );
 
 export default Api;
+
+
+// import axios from 'axios';
+
+// const Api = axios.create({
+//   baseURL: import.meta.env.VITE_API_BASE_URL,
+//   withCredentials: true,
+//   headers: {
+//     'Content-Type': 'application/json'
+//   }
+// });
+
+// const getTokenFromSessionStorage = () => {
+//   return sessionStorage.getItem('DTRMS_BY_M4RKBELLO');
+// };
+
+// const setAuthorizationToken = (config) => {
+//   const token = getTokenFromSessionStorage();
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// };
+
+// Api.interceptors.request.use(
+//   (config) => {
+//     if (config.method === 'post') {
+//       config.headers['Content-Type'] = 'application/json';
+//     }
+//     return setAuthorizationToken(config);
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default Api;
