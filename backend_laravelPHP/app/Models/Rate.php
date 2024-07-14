942e8ac64cac8e2d93e926f3bb522332fb5f6aeb
m4rkbello <markbello10041999@gmail.com>
2024-07-14 22:53:00 +0800

    BACKEND:  RATE MODEL

--- backend_laravelPHP/app/Models/Rate.php
+++ backend_laravelPHP/app/Models/Rate.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Rate extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'rate_amount_per_day',
+        'rate_name',
+        'rate_details',
+        'rate_description',
+        'rate_status_id',
+        'rate_department_id',
+        'rate_created_by',
+        'rate_updated_by',
+    ];
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class, 'rate_department_id');
+    }
+
+
+
+
 }
