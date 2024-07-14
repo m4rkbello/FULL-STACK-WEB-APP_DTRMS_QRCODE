4d176b7b6df3268690851aa57a5335deff39c442
m4rkbello <markbello10041999@gmail.com>
2024-07-14 16:30:14 +0800

    FRONTEND

--- frontend_reactJS/src/components/Auth/admin/Dashboard.jsx
+++ frontend_reactJS/src/components/Auth/admin/Dashboard.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>
+    <div><div className="flex w-52 flex-col gap-4">
+    <div className="skeleton h-32 w-full"></div>
+    <div className="skeleton h-4 w-28"></div>
+    <div className="skeleton h-4 w-full"></div>
+    <div className="skeleton h-4 w-full"></div>
+  </div>
       
     </div>
   )
