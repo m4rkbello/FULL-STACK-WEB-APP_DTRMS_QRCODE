bacbff7ca27c45237f660a6b773208294b199427
m4rkbello <markbello10041999@gmail.com>
2024-07-14 22:51:05 +0800

    BACKEND:  DEDUCTION MODEL

--- backend_laravelPHP/app/Models/Deduction.php
+++ backend_laravelPHP/app/Models/Deduction.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\HasMany;
 
 class Deduction extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'deduction_amount',
+        'deduction_name',
+        'deduction_description',
+        'deduction_created_by',
+        'deduction_updated_by',
+    ];
+
+
+
+
+
 }
