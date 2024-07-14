667a8ac2bd284b5896ec8ffd9d82402b459a7386
m4rkbello <markbello10041999@gmail.com>
2024-07-14 22:52:40 +0800

    BACKEND:  PAYROLL MODEL

--- backend_laravelPHP/app/Models/payroll.php
+++ backend_laravelPHP/app/Models/payroll.php
@@ -8,4 +8,55 @@ use Illuminate\Database\Eloquent\Model;
 class Payroll extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'payroll_total_amount',
+        'payroll_details',
+        'payroll_description',
+        'payroll_department_id',
+        'payroll_status_id',
+        'payroll_employee_id',
+        'payroll_rate_id',
+        'payroll_deduction_id',
+        'payroll_overtime_id',
+        'payroll_created_by',
+        'payroll_updated_by',
+    ];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class, 'payroll_employee_id');
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class, 'payroll_department_id');
+    }
+
+    public function rate()
+    {
+        return $this->belongsTo(Rate::class, 'payroll_rate_id');
+    }
+
+    public function deduction()
+    {
+        return $this->hasMany(Deduction::class, 'payroll_deduction_id');
+    }
+
+    public function overtime()
+    {
+        return $this->hasMany(Overtime::class, 'payroll_overtime_id');
+    }
+  
+
+
+    
+
+
+
 }
+
+
+
+
+
