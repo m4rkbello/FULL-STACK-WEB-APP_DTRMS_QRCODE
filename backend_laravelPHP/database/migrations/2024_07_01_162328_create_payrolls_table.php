870c6bd97e0fe740ea13b1d053abf1f31dca1468
m4rkbello <markbello10041999@gmail.com>
2024-07-14 23:33:42 +0800

    BACKEND:  CORRECTION FOR PAYROLL IF FK THE ID IT WON'T HAVE A LENGTH

--- backend_laravelPHP/database/migrations/2024_07_01_162328_create_payrolls_table.php
+++ backend_laravelPHP/database/migrations/2024_07_01_162328_create_payrolls_table.php
@@ -16,22 +16,21 @@ return new class extends Migration
             $table->decimal('payroll_total_amount', 10, 2); // 10 digits total, 2 decimal places
             $table->string('payroll_details', 255);
             $table->string('payroll_description', 255);
-            $table->integer('payroll_status_id',11)->nullable();
-            $table->integer('payroll_employee_id',11)->nullable();
-            $table->integer('payroll_department_id',11)->nullable();
-            $table->integer('payroll_rate_id',11)->nullable();
-            $table->integer('payroll_deduction_id',11)->nullable();
-            $table->integer('payroll_overtime_id',11)->nullable();
-            $table->integer('payroll_created_by',11)->nullable();
-            $table->integer('payroll_updated_by',11)->nullable();
+            $table->integer('payroll_status_id')->nullable();
+            $table->integer('payroll_employee_id')->nullable();
+            $table->integer('payroll_department_id')->nullable();
+            $table->integer('payroll_rate_id')->nullable();
+            $table->integer('payroll_deduction_id')->nullable();
+            $table->integer('payroll_overtime_id')->nullable();
+            $table->integer('payroll_created_by')->nullable();
+            $table->integer('payroll_updated_by')->nullable();
             $table->timestamps();
-            //FOREIGN-KEYS
+            // Foreign Keys
             $table->foreign('payroll_employee_id')->references('id')->on('employees');
             $table->foreign('payroll_department_id')->references('id')->on('departments');
             $table->foreign('payroll_rate_id')->references('id')->on('rates');
             $table->foreign('payroll_deduction_id')->references('id')->on('deductions');
             $table->foreign('payroll_overtime_id')->references('id')->on('overtimes');
-            
         });
     }
 
