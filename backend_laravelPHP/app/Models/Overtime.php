1b9d0d53c156f3239ed9c0fb6b70b6f4fd06534a
m4rkbello <markbello10041999@gmail.com>
2024-07-14 22:52:18 +0800

    BACKEND:  OVERTIME MODEL

--- backend_laravelPHP/app/Models/Overtime.php
+++ backend_laravelPHP/app/Models/Overtime.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Overtime extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'overtime_hour',
+        'overtime_rate_per_hour',
+        'overtime_name',
+        'overtime_description',
+        'overtime_created_by',
+        'overtime_updated_by',
+    ];
+
+
+
 }
