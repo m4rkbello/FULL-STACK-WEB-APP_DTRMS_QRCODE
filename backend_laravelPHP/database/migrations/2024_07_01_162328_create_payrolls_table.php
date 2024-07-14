1523a29c301ddec3e3a3dae4a7f5c0451ce103ba
m4rkbello <markbello10041999@gmail.com>
2024-07-14 14:30:34 +0800

    ADD BACKEND

--- backend_laravelPHP/database/migrations/2024_07_01_162328_create_payrolls_table.php
+++ backend_laravelPHP/database/migrations/2024_07_01_162328_create_payrolls_table.php
@@ -13,11 +13,18 @@ return new class extends Migration
     {
         Schema::create('payrolls', function (Blueprint $table) {
             $table->id();
-            $table->integer('payroll_data');
-            $table->string('payroll_details');
-            $table->string('payroll_description');
-            $table->string('payroll_department');
-            $table->integer('payroll_status_id')->nullable();
+            $table->decimal('payroll_total_amount', 10, 2); // 10 digits total, 2 decimal places
+            $table->string('payroll_details', 255);
+            $table->string('payroll_description', 255);
+            $table->string('payroll_department', 255);
+            $table->integer('payroll_status_id',11)->nullable();
+            $table->integer('payroll_attendance_employee_rate_id',11)->nullable();
+            $table->integer('payroll_employee_id',11)->nullable();
+            $table->integer('payroll_rate_id',11)->nullable();
+            $table->integer('payroll_deduction_id',11)->nullable();
+            $table->integer('payroll_overtime_id',11)->nullable();
+            $table->integer('payroll_created_by',11)->nullable();
+            $table->integer('payroll_updated_by',11)->nullable();
             $table->timestamps();
         });
     }
