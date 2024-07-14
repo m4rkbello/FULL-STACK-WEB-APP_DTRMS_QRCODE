797f8d067a4696975088b47eadd9b980fe7e171a
m4rkbello <markbello10041999@gmail.com>
2024-07-14 14:30:07 +0800

    ADD BACKEND

--- backend_laravelPHP/app/Models/payroll.php
+++ backend_laravelPHP/app/Models/payroll.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class payroll extends Model
+class Payroll extends Model
 {
     use HasFactory;
 }
