54c7cb9ebf2292b493467b841f8aa34b4a52a80a
m4rkbello <markbello10041999@gmail.com>
2024-07-15 23:55:04 +0800

    BACKEND: ERROR TIWASUNON

--- backend_laravelPHP/database/migrations/2024_07_01_162328_create_payrolls_table.php
+++ backend_laravelPHP/database/migrations/2024_07_01_162328_create_payrolls_table.php
@@ -4,28 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreatePayrollsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('payrolls', function (Blueprint $table) {
             $table->id();
-            $table->decimal('payroll_total_amount', 10, 2); // 10 digits total, 2 decimal places
             $table->string('payroll_details', 255);
+            $table->decimal('payroll_total_amount', 10, 2);
             $table->string('payroll_description', 255);
-            $table->integer('payroll_status_id')->nullable();
-            $table->integer('payroll_employee_id')->nullable();
-            $table->integer('payroll_department_id')->nullable();
-            $table->integer('payroll_rate_id')->nullable();
-            $table->integer('payroll_deduction_id')->nullable();
-            $table->integer('payroll_overtime_id')->nullable();
-            $table->integer('payroll_created_by')->nullable();
-            $table->integer('payroll_updated_by')->nullable();
+            $table->unsignedBigInteger('payroll_status_id')->nullable();
+            $table->unsignedBigInteger('payroll_employee_id')->nullable();
+            $table->unsignedBigInteger('payroll_department_id')->nullable();
+            $table->unsignedBigInteger('payroll_rate_id')->nullable();
+            $table->unsignedBigInteger('payroll_deduction_id')->nullable();
+            $table->unsignedBigInteger('payroll_overtime_id')->nullable();
+            $table->unsignedBigInteger('payroll_created_by')->nullable();
+            $table->unsignedBigInteger('payroll_updated_by')->nullable();
             $table->timestamps();
-            // Foreign Keys
+
             $table->foreign('payroll_employee_id')->references('id')->on('employees');
             $table->foreign('payroll_department_id')->references('id')->on('departments');
             $table->foreign('payroll_rate_id')->references('id')->on('rates');
@@ -34,11 +31,8 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('payrolls');
     }
-};
+}
