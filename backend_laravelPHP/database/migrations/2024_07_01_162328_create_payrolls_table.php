4bbf4cf8eb578049f246e0480d89c9c9f847b144
m4rkbello <markbello10041999@gmail.com>
2024-07-16 23:47:38 +0800

    BACKEND: ADD

--- backend_laravelPHP/database/migrations/2024_07_01_162328_create_payrolls_table.php
+++ backend_laravelPHP/database/migrations/2024_07_01_162328_create_payrolls_table.php
@@ -8,27 +8,29 @@ class CreatePayrollsTable extends Migration
 {
     public function up(): void
     {
-        Schema::create('payrolls', function (Blueprint $table) {
-            $table->id();
-            $table->string('payroll_details', 255);
-            $table->decimal('payroll_total_amount', 10, 2);
-            $table->string('payroll_description', 255);
-            $table->unsignedBigInteger('payroll_status_id')->nullable();
-            $table->unsignedBigInteger('payroll_employee_id')->nullable();
-            $table->unsignedBigInteger('payroll_department_id')->nullable();
-            $table->unsignedBigInteger('payroll_rate_id')->nullable();
-            $table->unsignedBigInteger('payroll_deduction_id')->nullable();
-            $table->unsignedBigInteger('payroll_overtime_id')->nullable();
-            $table->unsignedBigInteger('payroll_created_by')->nullable();
-            $table->unsignedBigInteger('payroll_updated_by')->nullable();
-            $table->timestamps();
+        if (!Schema::hasTable('payrolls')) {
+            Schema::create('payrolls', function (Blueprint $table) {
+                $table->id();
+                $table->string('payroll_details', 255);
+                $table->decimal('payroll_total_amount', 10, 2);
+                $table->string('payroll_description', 255);
+                $table->unsignedBigInteger('payroll_status_id')->nullable();
+                $table->unsignedBigInteger('payroll_employee_id')->nullable();
+                $table->unsignedBigInteger('payroll_department_id')->nullable();
+                $table->unsignedBigInteger('payroll_rate_id')->nullable();
+                $table->unsignedBigInteger('payroll_deduction_id')->nullable();
+                $table->unsignedBigInteger('payroll_overtime_id')->nullable();
+                $table->unsignedBigInteger('payroll_created_by')->nullable();
+                $table->unsignedBigInteger('payroll_updated_by')->nullable();
+                $table->timestamps();
 
-            $table->foreign('payroll_employee_id')->references('id')->on('employees');
-            $table->foreign('payroll_department_id')->references('id')->on('departments');
-            $table->foreign('payroll_rate_id')->references('id')->on('rates');
-            $table->foreign('payroll_deduction_id')->references('id')->on('deductions');
-            $table->foreign('payroll_overtime_id')->references('id')->on('overtimes');
-        });
+                $table->foreign('payroll_employee_id')->references('id')->on('employees')->onDelete('set null');
+                $table->foreign('payroll_department_id')->references('id')->on('departments')->onDelete('set null');
+                $table->foreign('payroll_rate_id')->references('id')->on('rates')->onDelete('set null');
+                $table->foreign('payroll_deduction_id')->references('id')->on('deductions')->onDelete('set null');
+                $table->foreign('payroll_overtime_id')->references('id')->on('overtimes')->onDelete('set null');
+            });
+        }
     }
 
     public function down(): void
