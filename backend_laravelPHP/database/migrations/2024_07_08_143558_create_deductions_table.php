01b431e517ad2d63bdd1d11e13897a786af94747
m4rkbello <markbello10041999@gmail.com>
2024-07-16 23:48:03 +0800

    BACKEND: ADD

--- backend_laravelPHP/database/migrations/2024_07_08_143558_create_deductions_table.php
+++ backend_laravelPHP/database/migrations/2024_07_08_143558_create_deductions_table.php
@@ -8,21 +8,17 @@ class CreateDeductionsTable extends Migration
 {
     public function up(): void
     {
-        Schema::create('deductions', function (Blueprint $table) {
-            $table->id();
-            $table->string('deduction_name', 255)->nullable();
-            $table->decimal('deduction_amount', 10, 2)->nullable();
-            $table->string('deduction_description', 255)->nullable();
-            $table->unsignedBigInteger('deduction_created_by')->nullable();
-            $table->unsignedBigInteger('deduction_updated_by')->nullable();
-            $table->timestamps();
-        });
-
-        // Ensure 'id' column is primary key and indexed
-        Schema::table('deductions', function (Blueprint $table) {
-            $table->primary('id');
-            $table->index('id');
-        });
+        if (!Schema::hasTable('deductions')) {
+            Schema::create('deductions', function (Blueprint $table) {
+                $table->id();
+                $table->string('deduction_name', 255)->nullable();
+                $table->decimal('deduction_amount', 10, 2)->nullable();
+                $table->string('deduction_description', 255)->nullable();
+                $table->unsignedBigInteger('deduction_created_by')->nullable();
+                $table->unsignedBigInteger('deduction_updated_by')->nullable();
+                $table->timestamps();
+            });
+        }
     }
 
     public function down(): void
