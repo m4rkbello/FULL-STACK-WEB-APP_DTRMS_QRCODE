7c3b27a639526d68740ec6b65a2b60a6a0c2b3a4
m4rkbello <markbello10041999@gmail.com>
2024-07-19 00:53:39 +0800

    add

--- frontend_reactJS/src/components/Auth/admin/Dashboard.jsx
+++ frontend_reactJS/src/components/Auth/admin/Dashboard.jsx
@@ -2,13 +2,69 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div><div className="flex w-52 flex-col gap-4">
-    <div className="skeleton h-32 w-full"></div>
-    <div className="skeleton h-4 w-28"></div>
-    <div className="skeleton h-4 w-full"></div>
-    <div className="skeleton h-4 w-full"></div>
-  </div>
-      
+    <div className="container glass mx-auto h-auto w-auto p-4">
+
+      <div className="grid grid-rows-2 grid-flow-col gap-8 pt-0 mt-0 pb-0 mb-0">
+        <div className="card glass w-auto h-3/4">
+          <figure>
+            <img src="https://i.ibb.co/Lp0X39G/vite.png" alt="vite" border="0" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Life hack</h2>
+            <p>How to park your car at your garage?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Learn now!</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card glass w-auto">
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              alt="car!" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Life hack</h2>
+            <p>How to park your car at your garage?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Learn now!</button>
+            </div>
+          </div>
+        </div>
+        
+        <div className="card glass w-auto">
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              alt="car!" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Life hack</h2>
+            <p>How to park your car at your garage?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Learn now!</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card glass w-auto">
+          <figure>
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+              alt="car!" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Life hack</h2>
+            <p>How to park your car at your garage?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Learn now!</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   )
 }
