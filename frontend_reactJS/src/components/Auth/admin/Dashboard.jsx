bc11315f0f9b8a19442ba909b72cfe0ce7a2930c
m4rkbello <markbello10041999@gmail.com>
2024-07-21 13:41:01 +0800

    FRONTEND: ADD CARDS FOR DEFAULT DATAS

--- frontend_reactJS/src/components/Auth/admin/Dashboard.jsx
+++ frontend_reactJS/src/components/Auth/admin/Dashboard.jsx
@@ -2,66 +2,70 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div className="container glass mx-auto h-auto w-auto p-4">
+    <div className="container glass mx-auto h-auto w-auto p-4 shadow-xl rounded-lg">
 
-      <div className="grid grid-rows-2 grid-flow-col gap-8 pt-0 mt-0 pb-0 mb-0">
-        <div className="card glass w-auto h-3/4">
-          <figure>
-            <img src="https://i.ibb.co/Lp0X39G/vite.png" alt="vite" border="0" />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Life hack</h2>
-            <p>How to park your car at your garage?</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Learn now!</button>
-            </div>
-          </div>
-        </div>
+      <div className="grid grid-rows-2 grid-flow-col gap-8 pt-0 mt-0 pb-0 mb-0 shadow-xl rounded-lg">
 
-        <div className="card glass w-auto">
+        <div className="card card-side bg-base-100 shadow-xl">
           <figure>
             <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="car!" />
+              src="https://i.ibb.co/xL6Mbx0/m4rk.png"
+              alt="Movie" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">Life hack</h2>
-            <p>How to park your car at your garage?</p>
+            <h2 className="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
             <div className="card-actions justify-end">
-              <button className="btn btn-primary">Learn now!</button>
-            </div>
-          </div>
-        </div>
-        
-        <div className="card glass w-auto">
-          <figure>
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="car!" />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Life hack</h2>
-            <p>How to park your car at your garage?</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Learn now!</button>
+              <button className="btn btn-primary">Watch</button>
             </div>
           </div>
         </div>
 
-        <div className="card glass w-auto">
+        <div className="card card-side bg-base-100 shadow-xl">
           <figure>
             <img
-              src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="car!" />
+              src="https://i.ibb.co/xL6Mbx0/m4rk.png"
+              alt="Movie" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">Life hack</h2>
-            <p>How to park your car at your garage?</p>
+            <h2 className="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
             <div className="card-actions justify-end">
-              <button className="btn btn-primary">Learn now!</button>
+              <button className="btn btn-primary">Watch</button>
             </div>
           </div>
         </div>
+
+        <div className="card card-side bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co/xL6Mbx0/m4rk.png"
+              alt="Movie" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="card card-side bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co/xL6Mbx0/m4rk.png"
+              alt="Movie" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">New movie is released!</h2>
+            <p>Click the button to watch on Jetflix app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Watch</button>
+            </div>
+          </div>
+        </div>
+
       </div>
 
 
