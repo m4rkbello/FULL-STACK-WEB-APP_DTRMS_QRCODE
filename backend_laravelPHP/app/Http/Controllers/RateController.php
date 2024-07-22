e5a4bf9fb8999e7e46470c53f94f7707dd77f3f3
m4rkbello <markbello10041999@gmail.com>
2024-07-22 22:14:04 +0800

    BACKEND: RATE - ADD DATA

--- backend_laravelPHP/app/Http/Controllers/RateController.php
+++ backend_laravelPHP/app/Http/Controllers/RateController.php
@@ -8,6 +8,7 @@ use Illuminatapie\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\File;
+use Carbon\Carbon;
 
 
 class RateController extends Controller
@@ -46,7 +47,45 @@ class RateController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $data = $request->validate([
+                'rate_name' => 'required|string',
+                'employee_amount_per_day' => 'required|integer',
+                'rate_details' => 'required|string',
+                'rate_description' => 'required|string',
+                'rate_status_id' => 'required|integer',
+                'rate_department_id' => 'required|string',
+            ]);
+    
+            $rates = Rate::create([
+                'rate_name' => $data['rate_name'],
+                'employee_amount_per_day' => $data['employee_amount_per_day'],
+                'rate_details' => $data['rate_details'],
+                'rate_description' => $data['rate_description'],
+                'rate_status_id' => $data['rate_status_id'],
+                'rate_department_id' => $data['rate_department_id'],
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+    
+            $response_data = [
+                'status' => 201,
+                'success' => true,
+                'message' => 'Rate has successfully created!',
+                'details' => $rates,
+            ];
+    
+            return response($response_data, 201);
+        } catch (\Exception $error) {
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Rate has not successfully created!',
+                'status' => 401,
+                'errors' => $error,
+            ], 401);
+            
+        } 
     }
 
     /**
