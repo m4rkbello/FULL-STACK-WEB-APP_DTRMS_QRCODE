2cd5e8fe45a92a10a1f0e26f6ace74d1ca2cfaca
m4rkbello <markbello10041999@gmail.com>
2024-07-25 22:03:58 +0800

    BACKEND: ADD ANOTTHER COLUMN FIELD

--- backend_laravelPHP/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend_laravelPHP/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->bigInteger('user_contact_no');
             $table->string('user_password');
-            $table->integer('user_type')->nullable();
+            $table->integer('user_type_id')->nullable();
             $table->string('user_image')->nullable();
             $table->rememberToken();
             $table->timestamps();
