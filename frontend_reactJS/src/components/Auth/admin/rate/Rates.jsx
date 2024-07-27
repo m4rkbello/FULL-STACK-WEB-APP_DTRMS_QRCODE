2a641a8a2419af9f13d1dbef04abc8c8021d9efd
m4rkbello <markbello10041999@gmail.com>
2024-07-27 20:45:08 +0800

    FRONTEND: ADD SOMETHING

--- frontend_reactJS/src/components/Auth/admin/rate/Rates.jsx
+++ frontend_reactJS/src/components/Auth/admin/rate/Rates.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 
 const Rates = () => {
+
   return (
-    <div>
-    <div className="diff aspect-[16/9]">
-    <div className="diff-item-1">
-      <div className="bg-primary text-primary-content grid place-content-center text-9xl font-black">
-        MARKBELLO
-      </div>
-    </div>
-    <div className="diff-item-2">
-      <div className="bg-base-200 grid place-content-center text-9xl font-black">DAISY</div>
-    </div>
-    <div className="diff-resizer"></div>
-  </div>
-    </div>
+          <div className='h-full max-h-full w-full max-w-full glass mx-auto p-4 '>
+            <div className="hero" style={{ backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)", }}>
+              <div className="hero-overlay bg-opacity-60"></div>
+              <div className="hero-content text-neutral-content text-center">
+                <div className="max-w-md">
+                  <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                  <p className="mb-5">
+                    Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                    quasi. In deleniti eaque aut repudiandae et a id nisi.
+                  </p>
+                  <button className="btn btn-primary">Get Started</button>
+                </div>
+              </div>
+            </div>
+          </div>
   )
 }
 
-export default Rates
+export default Rates;
