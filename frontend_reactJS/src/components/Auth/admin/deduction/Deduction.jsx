fe6a6a7ffd642594d46aab5c3277c618f98e3334
m4rkbello <markbello10041999@gmail.com>
2024-07-27 20:54:40 +0800

    FRONTEND: ADD SOMETHING FOR FRONTEND

---
+++ frontend_reactJS/src/components/Auth/admin/deduction/Deduction.jsx
@@ -0,0 +1,23 @@
+import react from 'react'
+
+const Deduction = () => {
+    return (
+        <div className='h-full max-h-full w-full max-w-full glass mx-auto p-4 '>
+        <div className="hero" style={{ backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)", }}>
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-neutral-content text-center">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+              <p className="mb-5">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                quasi. In deleniti eaque aut repudiandae et a id nisi.
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+
+export default Deduction;
