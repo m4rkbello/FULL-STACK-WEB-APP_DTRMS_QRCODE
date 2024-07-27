28fb04b3fa777f8f5955e7e2e004a1429448724b
m4rkbello <markbello10041999@gmail.com>
2024-07-27 20:55:52 +0800

    FRONTEND: ADD SOMETHING FOR FRONTEND

--- frontend_reactJS/src/components/Auth/admin/overtime/Overtime.jsx
+++ frontend_reactJS/src/components/Auth/admin/overtime/Overtime.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 const Overtime = () => {
   return (
-    <div>
-    
+    <div className='h-full max-h-full w-full max-w-full glass mx-auto p-4 '>
+      <div className="hero" style={{ backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)", }}>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+              quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
