1f802a385004e7eedea609404ed398d161cdd7d4
m4rkbello <markbello10041999@gmail.com>
2024-07-27 20:42:23 +0800

    FRONTEND: ADD SOMETHING

--- frontend_reactJS/src/components/Auth/admin/payroll/Payroll.jsx
+++ frontend_reactJS/src/components/Auth/admin/payroll/Payroll.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 
 const Payroll = () => {
   return (
-    <div>
-    <div className="diff aspect-[16/9]">
-    <div className="diff-item-1">
-      <div className="bg-primary text-primary-content grid place-content-center text-9xl font-black">
-        MARKBELLO
+    <div className='container glass mx-auto p-4'>
+      <div className="hero" style={{ backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)", }}>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+              quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
       </div>
-    </div>
-    <div className="diff-item-2">
-      <div className="bg-base-200 grid place-content-center text-9xl font-black">DAISY</div>
-    </div>
-    <div className="diff-resizer"></div>
-  </div>
     </div>
   )
 }
