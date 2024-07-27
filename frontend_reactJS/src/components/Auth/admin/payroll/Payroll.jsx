b08d80e02d6bca83815c0c9e3a10f8385bab9632
m4rkbello <markbello10041999@gmail.com>
2024-07-27 19:02:23 +0800

    FRONTEND: ADD FILE

--- frontend_reactJS/src/components/Auth/admin/payroll/Payroll.jsx
+++ frontend_reactJS/src/components/Auth/admin/payroll/Payroll.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Payroll = () => {
   return (
-    <div>Payroll</div>
+    <div>
+    <div className="diff aspect-[16/9]">
+    <div className="diff-item-1">
+      <div className="bg-primary text-primary-content grid place-content-center text-9xl font-black">
+        MARKBELLO
+      </div>
+    </div>
+    <div className="diff-item-2">
+      <div className="bg-base-200 grid place-content-center text-9xl font-black">DAISY</div>
+    </div>
+    <div className="diff-resizer"></div>
+  </div>
+    </div>
   )
 }
 
