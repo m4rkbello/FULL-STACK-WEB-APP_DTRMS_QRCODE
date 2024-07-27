611296558e30700617a3d2a2dac5ef23976c0942
m4rkbello <markbello10041999@gmail.com>
2024-07-27 19:00:38 +0800

    FRONTEND: ADD FILE

---
+++ frontend_reactJS/src/components/Auth/admin/overtime/DeactivateOvertime.jsx
@@ -0,0 +1,18 @@
+import react from 'react'
+
+const DeactivateOvertime = () => {
+    return(
+        <div>
+        <div className="diff aspect-[16/9]">
+        <div className="diff-item-1">
+          <div className="bg-primary text-primary-content grid place-content-center text-9xl font-black">
+            MARKBELLO
+          </div>
+        </div>
+        <div className="diff-item-2">
+          <div className="bg-base-200 grid place-content-center text-9xl font-black">DAISY</div>
+        </div>
+        <div className="diff-resizer"></div>
+      </div></div>
+    )
+}
