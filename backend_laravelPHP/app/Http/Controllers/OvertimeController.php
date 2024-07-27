6f91804ccf8582d6a829b1c909775f46cdd2653a
m4rkbello <markbello10041999@gmail.com>
2024-07-27 17:23:37 +0800

    BACKEND: ADD OVERTIMES ADD FUNCTION API-ROUTE AND FUNCTION QUERY

--- backend_laravelPHP/app/Http/Controllers/OvertimeController.php
+++ backend_laravelPHP/app/Http/Controllers/OvertimeController.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\File;
+use Carbon\Carbon;
 
 class OvertimeController extends Controller
 {
@@ -43,7 +44,62 @@ class OvertimeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $data = $request->validate([
+                'overtime_name' => 'required|string',
+                'overtime_hour' => 'required|integer',
+                'overtime_rate_per_hour' => 'required|integer',
+                'overtime_description' => 'required|string',
+                'overtime_status_id' => 'required|integer',
+            ]);
+
+            $overtime_collection = Overtime::create([
+                'overtime_name' => $data['overtime_name'],
+                'overtime_hour' => $data['overtime_hour'],
+                'overtime_rate_per_hour' => $data['overtime_rate_per_hour'],
+                'overtime_description' => $data['overtime_description'],
+                'overtime_status_id' => $data['overtime_status_id'],
+                'created_by' => auth()->id(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+
+            $response_data = [
+                'status' => 201,
+                'success' => true,
+                'message' => 'Overtime has successfully created!',
+                'data' => $overtime_collection,
+            ];
+
+            return response($response_data, 201);
+
+        } catch (ValidationException $e) {
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Validation failed',
+                'status' => 422,
+                'errors' => $e->errors(),
+            ], 422);
+
+        } catch (ModelNotFoundException $e) {
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Rate not found',
+                'status' => 404,
+            ], 404);
+
+        } catch (\Exception $error) {
+
+            return response()->json([
+                'success' => false,
+                'message' => 'An error occurred',
+                'status' => 500,
+                'errors' => $error->getMessage(),
+            ], 500);
+            
+        }
     }
 
     /**
@@ -114,7 +170,7 @@ class OvertimeController extends Controller
             \Log::error('Error in search method: ' . $e->getMessage(), ['exception' => $e]);
 
             // Return a more detailed error message only if in debug mode
-            $errorMessage = config('app.debug') ? $e->getMessage() : 'Failed to search deduction. Please try again later.';
+            $errorMessage = config('app.debug') ? $e->getMessage() : 'Failed to search overtime. Please try again later.';
 
             return response()->json([
                 'success' => false,
