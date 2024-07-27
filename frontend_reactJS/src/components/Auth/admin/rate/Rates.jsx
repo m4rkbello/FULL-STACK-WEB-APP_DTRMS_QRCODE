bcf30224c59474996333f12c6f1d13056377937a
m4rkbello <markbello10041999@gmail.com>
2024-07-27 19:04:43 +0800

    FRONTEND: ADD FILE

---
+++ frontend_reactJS/src/components/Auth/admin/rate/Rates.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Rates = () => {
+  return (
+    <div>
+    <div className="diff aspect-[16/9]">
+    <div className="diff-item-1">
+      <div className="bg-primary text-primary-content grid place-content-center text-9xl font-black">
+        MARKBELLO
+      </div>
+    </div>
+    <div className="diff-item-2">
+      <div className="bg-base-200 grid place-content-center text-9xl font-black">DAISY</div>
+    </div>
+    <div className="diff-resizer"></div>
+  </div>
+    </div>
+  )
+}
+
+export default Rates
