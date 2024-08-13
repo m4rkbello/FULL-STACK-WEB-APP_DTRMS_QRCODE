3a7eead3f7bec151b1fdf99ab6cb0c61bdaf2b4b
m4rkbello <markbello10041999@gmail.com>
2024-08-13 21:18:53 +0800

    FRONTEND: ADD ALL CHANGES

--- frontend_reactJS/src/components/Auth/admin/rate/EditRates.jsx
+++ frontend_reactJS/src/components/Auth/admin/rate/EditRates.jsx
@@ -4,7 +4,10 @@ const EditRates = () => {
   return (
     <div className='h-full max-h-full w-full max-w-full glass mx-auto p-4 shadow-slate-900/100'>
         
-        </div>
+
+
+        
+      </div>
  
   )
 }
