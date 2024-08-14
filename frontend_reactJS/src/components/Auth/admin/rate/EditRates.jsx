30cdf801213b1ace07940607d58a27f2d4b57c48
m4rkbello <markbello10041999@gmail.com>
2024-08-14 22:05:45 +0800

    FRONTEND: UPDATE RATES COMPONENT WITH DISPATCH UPDATE

--- frontend_reactJS/src/components/Auth/admin/rate/EditRates.jsx
+++ frontend_reactJS/src/components/Auth/admin/rate/EditRates.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
 // ICONS
-import { FcFolder, FcOpenedFolder, FcPrevious } from "react-icons/fc";
+import { FcFolder, FcOpenedFolder, FcPrevious, FcOk } from "react-icons/fc";
 // REDUX ACTIONS
 import { fetchRates, updateRate } from '../../../redux/actions/rateAction';
 
@@ -45,33 +45,119 @@ const EditRates = ({ fetchRates, updateRate, ratesData, loading }) => {
           </ul>
         </div>
       </div>
-      
+
       <div>
         {loading ? (
           <p>Loading...</p>
         ) : filteredRates.length > 0 ? (
-          <table className="min-w-full table-auto">
-            <thead>
-              <tr>
-                <th className="px-4 py-2">Rate Name</th>
-                <th className="px-4 py-2">Description</th>
-                <th className="px-4 py-2">Rate Amount</th>
-                {/* <th className="px-4 py-2">Created At</th> */}
-                {/* Add more columns as necessary */}
-              </tr>
-            </thead>
-            <tbody>
-              {filteredRates.map(rate => (
-                <tr key={rate.id}>
-                  <td className="border px-4 py-2">{rate.rate_name}</td>
-                  <td className="border px-4 py-2">{rate.rate_description}</td>
-                  <td className="border px-4 py-2">{rate.rate_amount_per_day}</td>
-                  {/* <td className="border px-4 py-2">{new Date(rate.created_at).toLocaleDateString()}</td> */}
-                  {/* Add more fields as necessary */}
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <div className="modal-action">
+          <form method="dialog" 
+          // onSubmit={handleSubmitAddRateData}
+          >
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 glass" 
+            // onClick={onClose}
+            >✕</button>
+            <div className="grid grid-cols-3 gap-8">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text text-black text-2xl">Rate Name</span>
+                </label>
+                <input
+                  name="rate_name"
+                  type="text"
+                  // value={formData.rate_name}
+                  // onChange={handleChange}
+                  placeholder="Enter rate name"
+                  className="input input-bordered glass shadow-2xl text-2xl text-black border-glass shadow-slate-900/100 custom-placeholder-text-color"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text text-black text-2xl">Rate Amount</span>
+                </label>
+                <input
+                  name="rate_amount_per_day"
+                  type="text"
+                  // value={formData.rate_amount_per_day}
+                  // onChange={handleChange}
+                  placeholder="Enter rate amount"
+                  className="input input-bordered shadow-3xl glass text-2xl text-black border-1 border-glass shadow-slate-900/100 custom-placeholder-text-color"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text text-black text-2xl">Rate Details</span>
+                </label>
+                <input
+                  name="rate_details"
+                  type="text"
+                  // value={formData.rate_details}
+                  // onChange={handleChange}
+                  placeholder="Enter rate details"
+                  className="input input-bordered shadow-2xl glass text-2xl text-black border-1 border-glass shadow-slate-900/100 custom-placeholder-text-color"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text text-black text-2xl">Rate Description</span>
+                </label>
+                <input
+                  name="rate_description"
+                  type="text"
+                  // value={formData.rate_description}
+                  // onChange={handleChange}
+                  placeholder="Enter rate description"
+                  className="input input-bordered shadow-2xl glass text-2xl text-black border-1 border-glass shadow-slate-900/100 custom-placeholder-text-color"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text text-black text-2xl">Rate Department</span>
+                </label>
+                <select
+                  name="rate_department_id"
+                  // value={formData.rate_department_id}
+                  // onChange={handleChange}
+                  className="input input-bordered shadow-2xl glass text-2xl text-black border-1 border-glass rounded-se-3xl shadow-slate-900/100 custom-placeholder-text-color"
+                >
+                  <option value="">Select Department</option>
+                  {/* {departments.map((department) => (
+                    <option key={department.id} value={department.id}>
+                      {department.department_name}
+                    </option>
+                  ))} */}
+
+                </select>
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text text-glass text-2xl">Rate Status</span>
+                </label>
+                <select
+                  name="rate_status_id"
+                  // value={formData.rate_status_id}
+                  // onChange={handleChange}
+                  className="input input-bordered shadow-2xl glass text-2xl text-black border-1 border-glass rounded-se-3xl shadow-slate-900/100 custom-placeholder-text-color"
+                >
+                  <option value="1">Active</option>
+                  <option value="0">Inactive</option>
+                </select>
+              </div>
+            </div>
+            <br />
+            <div className="flex">
+              <div>
+                <button
+                  type="submit"
+                  className="btn glass hover:text-white hover:bg-indigo-400"
+                  style={{ fontSize: "40px", color: "transparent", border: "none", backgroundColor: "transparent" }}
+                >
+                  <FcOk type="submit" style={{ fontSize: "40px", color: "transparent" }} className='text-black hover:text-black' />
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
         ) : (
           <p>No rates found for the provided ID.</p>
         )}
