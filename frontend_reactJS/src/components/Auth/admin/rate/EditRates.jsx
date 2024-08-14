c883ad9b2cda67e8b6270b09e38848660c023bed
m4rkbello <markbello10041999@gmail.com>
2024-08-14 21:19:51 +0800

    FRONTEND: EDIT RATES FOR UPDATE USING DESTRUCTURING

--- frontend_reactJS/src/components/Auth/admin/rate/EditRates.jsx
+++ frontend_reactJS/src/components/Auth/admin/rate/EditRates.jsx
@@ -1,15 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
+//ICONS
+import { FcFolder, FcOpenedFolder, FcPlus, FcSalesPerformance, FcSearch, FcPrevious, FcViewDetails, FcEmptyTrash, FcNext } from "react-icons/fc";
+//REDUXISM
+import { fetchRates, updateRate} from '../../../redux/actions/rateAction';
 
-const EditRates = () => {
+
+
+const EditRates = ({fetchRates, updateRate}) => {
+const rateId = useParams();
+console.log("DATA SA rateId", rateId);
+
+  
+  
+  useEffect(() => { 
+    fetchRates(); 
+  },[]);
+  
+
+
   return (
     <div className='h-full max-h-full w-full max-w-full glass mx-auto p-4 shadow-slate-900/100'>
-        
-
-
-        
+      <div className="flex flex-col bg-transparent mb-10 shadow-slate-900/100" >
+        <div className="flex items-center text-sm breadcrumbs">
+          <ul className="flex space-x-4">
+            <li>
+              <Link to="/" className='flex items-center hover:text-white'>
+                <FcPrevious style={{ height: "2rem", width: "2rem" }} />
+                <span className="ml-2">Home</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/employee/dashboard" className='flex items-center hover:text-white'>
+                <FcFolder style={{ height: "2rem", width: "2rem" }} />
+                <span className="ml-2">Rates</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="" className='flex items-center hover:text-white'>
+                <FcOpenedFolder style={{ height: "2rem", width: "2rem" }} />
+                <span className="ml-2">Data</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
- 
+    </div>
   )
 }
 
-export default EditRates
+
+const mapStateToProps = (state) => {
+  return {
+    ratesData: state.rateState,
+    loading: state.rateState.loading,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchRates: () => dispatch(fetchRates()),
+    updateRate: (UpdateRateData) => dispatch(updateRate(UpdateRateData)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditRates);
