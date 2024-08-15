c58d3b823baf4657eee9acf45242119d922269b5
m4rkbello <markbello10041999@gmail.com>
2024-08-15 21:16:23 +0800

    ADD IMAGE

--- frontend_reactJS/src/components/Auth/admin/Login.jsx
+++ frontend_reactJS/src/components/Auth/admin/Login.jsx
@@ -27,7 +27,7 @@ const Login = ({ loginUser }) => {
       setTimeout(() => {
         navigate("/"); // Redirect to root path upon successful login
         window.location.reload();
-      }, 300); // Adjust the timeout duration as needed
+      }, 5000); // Adjust the timeout duration as needed
     } catch (error) {
       setIsLoading(false);
       toast.error("Failed to log in. Please try again."); // Use toast for error notification
