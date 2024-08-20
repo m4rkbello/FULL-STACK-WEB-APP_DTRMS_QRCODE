9526d3d6331386f1ff20b39182f0b87eb7ac2160
m4rkbello <markbello10041999@gmail.com>
2024-08-20 23:31:18 +0800

    FROTEND: ADD SOMETHING FOR OVERTIMES

--- frontend_reactJS/src/components/Auth/admin/overtime/Overtime.jsx
+++ frontend_reactJS/src/components/Auth/admin/overtime/Overtime.jsx
@@ -5,63 +5,60 @@
 import React, { useEffect, useState } from 'react';
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
+/* eslint-disable react/prop-types */
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable no-unused-vars */
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { FaEye } from "react-icons/fa6";
-import { MdAutoDelete } from "react-icons/md";
-import { IoIosPersonAdd } from "react-icons/io";
-import { HiStatusOnline } from "react-icons/hi";
-import { MdOutlineNoAccounts } from "react-icons/md";
-import { RiAccountPinCircleFill } from "react-icons/ri";
-import { IoMdCloseCircle } from "react-icons/io";
-import { FaUserEdit, FaSave, FaLongArrowAltLeft } from "react-icons/fa";
-import { IoSearch } from "react-icons/io5";
-import { SiMicrosoftexcel } from "react-icons/si";
-import { IoIosPrint } from "react-icons/io";
-import { FcPrint, FcDataSheet, FcPlus, FcSearch, FcFolder, FcFile, FcCheckmark, FcViewDetails, FcEmptyTrash, FcCancel } from "react-icons/fc";
-//REDUX-ACTION-DISPATCH
+import { Link, useParams } from 'react-router-dom';
+//ICONS
+import { FcFolder, FcOpenedFolder, FcPlus, FcSalesPerformance, FcSearch, FcPrevious, FcViewDetails, FcEmptyTrash, FcNext } from "react-icons/fc";
+//REDUXISM
+
+//TOASTER
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Overtime = () => {
   return (
-    <div className='h-full max-h-full w-full max-w-full glass mx-auto p-4 '>
-      <div className="flex flex-wrap">
-        <div>
-          <div className="text-sm breadcrumbs mb-10 bg-transparent">
-            <ul>
-              <li>
-                <FcFolder
-                  style={{
-
-                    boxShadow: "0 10px 15px rgba(4, 4, 4, 0.23)",
-                  }}
-                />
-                <Link to="/" className='hover:text-white'>Home</Link>
-              </li>
-              <li>
-                <FcFolder
-                  style={{
-
-                    boxShadow: "0 10px 15px rgba(4, 4, 4, 0.23)",
-                  }}
-                />
-                <Link to="/employee/dashboard" className='hover:text-white'>Employee Dashboard</Link>
-              </li>
-              <li>
-                <span className="inline-flex gap-2 items-center">
-                  <FcFile
-                    style={{
-
-                      boxShadow: "0 10px 15px rgba(4, 4, 4, 0.23)",
-                    }}
-                  />
-                  <Link to="" className='hover:text-white'>Employee Personal Details</Link>
-                </span>
-              </li>
-            </ul>
-          </div>
-        </div>
+    <div className='h-full max-h-full w-full max-w-full glass mx-auto p-4 shadow-slate-900/100 rounded-t-lg rounded-b-lg rounded-l-lg rounded-r-lg'>
+    <div className="flex flex-col bg-transparent mb-10 shadow-slate-900/100" >
+      <div className="flex items-center text-sm breadcrumbs">
+        <ul className="flex space-x-4">
+          <li>
+            <Link to="/" className='flex items-center hover:text-white'>
+              <FcPrevious style={{ height: "2rem", width: "2rem" }} />
+              <span className="ml-2">Home</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/employee/dashboard" className='flex items-center hover:text-white'>
+              <FcFolder
+                style={{ height: "2rem", width: "2rem" }} />
+              <span className="ml-2">Rates</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="" className='flex items-center hover:text-white'>
+              <FcOpenedFolder style={{ height: "2rem", width: "2rem" }} />
+              <span className="ml-2">Data</span>
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
+    <div className="diff aspect-[16/9] shadow-xl">
+      <div className="diff-item-1">
+        <div className="glass text-primary-content grid place-content-center text-9xl font-black shadow-xl">
+
+         
+        </div>
+      </div>
+      <div className="diff-item-2">
+        <div className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% grid place-content-center text-7xl font-black shadow-xl">BAR GRAPH</div>
+      </div>
+      <div className="diff-resizer"></div>
+    </div>
+  </div>
   )
 }
 
