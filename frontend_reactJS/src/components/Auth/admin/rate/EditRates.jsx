481f2e0250443b17fa5e9ef78441ade96e2ae756
m4rkbello <markbello10041999@gmail.com>
2024-08-24 20:07:42 +0800

    FRONTEND: ADD SOMETHING COMMENT

--- frontend_reactJS/src/components/Auth/admin/rate/EditRates.jsx
+++ frontend_reactJS/src/components/Auth/admin/rate/EditRates.jsx
@@ -14,6 +14,7 @@ import { fetchDepartments } from '../../../redux/actions/departmentAction';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+//DISTRUCTURING
 const EditRates = ({ fetchRates, updateRate, ratesData, departmentData, fetchDepartments, loading }) => {
   //id sa rate.id
   const { rateId } = useParams();
