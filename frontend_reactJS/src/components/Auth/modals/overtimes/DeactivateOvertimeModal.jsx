d578b3d84b53f122961fe110451ccf2dbf5f904d
m4rkbello <markbello10041999@gmail.com>
2024-08-24 17:34:22 +0800

    FRONTEND: OVERTIME!

--- frontend_reactJS/src/components/Auth/modals/overtimes/DeactivateOvertimeModal.jsx
+++ frontend_reactJS/src/components/Auth/modals/overtimes/DeactivateOvertimeModal.jsx
@@ -3,36 +3,33 @@
 import React from 'react';
 import { FcOk } from 'react-icons/fc';
 
-
 const DeactivateOvertimeModal = ({ isOpen, onClose, deactivateOvertime }) => {
-    if (!isOpen) return null;
-  
-    const handleDeactivate = () => {
-      deactivateOvertime();
-      onClose();
-    };
-  
-    return (
-      <dialog open className="modal border border-black">
-        <div className="modal-box bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% border border-black relative">
-          <form method="dialog">
-            <button type="button" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={onClose}>✕</button>
-          </form>
-          <span className="font-bold text-3xl text-black">REMOVE OVERTIME</span>
-          <p className="py-4 text-2xl">Are you sure you want to remove this item?</p>
-          <div className="flex">
-            <button 
-              className="btn hover:text-white hover:bg-indigo-400" 
-              style={{ fontSize: "40px", color: "transparent", border: "none", backgroundColor: "transparent" }} 
-              onClick={handleDeactivate}
-            >
-              <FcOk style={{ fontSize: "40px" }} className='text-lime-400 hover:text-black' />
-            </button>
-          </div>
+  if (!isOpen) return null;
+
+  const handleDeactivate = () => {
+    deactivateOvertime();
+  };
+
+  return (
+    <dialog open className="modal border border-black">
+      <div className="modal-box bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% border border-black relative">
+        <form method="dialog">
+          <button type="button" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={onClose}>✕</button>
+        </form>
+        <span className="font-bold text-3xl text-black">REMOVE OVERTIME</span>
+        <p className="py-4 text-2xl">Are you sure you want to remove this item?</p>
+        <div className="flex">
+          <button 
+            className="btn hover:text-white hover:bg-indigo-400" 
+            style={{ fontSize: "40px", color: "transparent", border: "none", backgroundColor: "transparent" }} 
+            onClick={handleDeactivate}
+          >
+            <FcOk style={{ fontSize: "40px" }} className='text-lime-400 hover:text-black' />
+          </button>
         </div>
-      </dialog>
-    );
-  }
-  
+      </div>
+    </dialog>
+  );
+};
 
 export default DeactivateOvertimeModal;
