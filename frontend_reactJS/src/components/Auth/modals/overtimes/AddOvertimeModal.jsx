6250effb55d4ee8633b2398b85f49cea34f2b451
m4rkbello <markbello10041999@gmail.com>
2024-08-25 17:00:36 +0800

    FRONTEND: ADD SOMETHING!

--- frontend_reactJS/src/components/Auth/modals/overtimes/AddOvertimeModal.jsx
+++ frontend_reactJS/src/components/Auth/modals/overtimes/AddOvertimeModal.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
