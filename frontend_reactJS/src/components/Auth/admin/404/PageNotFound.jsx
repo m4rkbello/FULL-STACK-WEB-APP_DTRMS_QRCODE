c3809f31a3918f279564325590c10c27b94d803b
m4rkbello <markbello10041999@gmail.com>
2024-08-30 21:04:49 +0800

    FRONTEND: ADD

---
+++ frontend_reactJS/src/components/Auth/admin/404/PageNotFound.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+   
+    <div className="diff aspect-[16/9] rounded-l-lg" >
+    <div className="diff-item-1">
+      <div className="text-primary-content grid place-content-center text-9xl font-black bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
+        DTRMS+
+      </div>
+    </div>
+    <div className="diff-item-2 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
+      <div className="bg-black grid place-content-center text-7xl font-black text-sky-500">BY M4RKBELLO</div>
+    </div>
+    <div className="diff-resizer">BY M4RKBELLO</div>
+  </div>
+
+
+  )
+}
+
+export default PageNotFound;
