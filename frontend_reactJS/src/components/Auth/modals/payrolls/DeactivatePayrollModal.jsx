c5fe703abf5e14373ad83b29e27334181bc70766
m4rkbello <markbello10041999@gmail.com>
2024-09-01 23:41:54 +0800

    TUGPA NAKO

---
+++ frontend_reactJS/src/components/Auth/modals/payrolls/DeactivatePayrollModal.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const DeactivatePayrollModal = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DeactivatePayrollModal
