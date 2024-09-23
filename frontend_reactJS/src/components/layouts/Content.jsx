2109fb005c694ade4fec8510808591fb469cb7c7
m4rkbello <markbello10041999@gmail.com>
2024-09-23 22:27:46 +0800

    ADD

--- frontend_reactJS/src/components/layouts/Content.jsx
+++ frontend_reactJS/src/components/layouts/Content.jsx
@@ -4,10 +4,7 @@ import React from 'react'
 const Content = () => {
   return (
     <div className="p-4 bg-white rounded-lg shadow-lg">
-    <span className="loading bg-black loading-infinity loading-xs"></span>
-    <span className="loading  bg-black loading-infinity loading-sm"></span>
-    <span className="loading bg-black loading-infinity loading-md"></span>
-    <span className="loading bg-black loading-infinity loading-lg"></span>
+ 
     </div>
   )
 }
