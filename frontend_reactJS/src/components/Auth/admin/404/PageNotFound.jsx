4df30a5060efe3ece4b80ff0421fdbbdd75cb056
m4rkbello <markbello10041999@gmail.com>
2024-10-06 23:04:29 +0800

    ADD NEW COLOR

--- frontend_reactJS/src/components/Auth/admin/404/PageNotFound.jsx
+++ frontend_reactJS/src/components/Auth/admin/404/PageNotFound.jsx
@@ -6,11 +6,11 @@ const PageNotFound = () => {
    
     <div className="diff aspect-[16/9] rounded-l-lg" >
     <div className="diff-item-1">
-      <div className="text-primary-content grid place-content-center text-9xl font-black bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
+      <div className="text-primary-content grid place-content-center text-9xl font-black bg-gradient-to-r from-violet-700 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
         DTRMS+
       </div>
     </div>
-    <div className="diff-item-2 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
+    <div className="diff-item-2 bg-gradient-to-r from-violet-700 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
       <div className="bg-black grid place-content-center text-7xl font-black text-sky-500">BY M4RKBELLO</div>
     </div>
     <div className="diff-resizer">BY M4RKBELLO</div>
