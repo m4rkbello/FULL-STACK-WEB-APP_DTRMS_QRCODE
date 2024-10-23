41bde3afa668facbe77cb59fb0233339d4bf1826
m4rkbello <markbello10041999@gmail.com>
2024-10-23 20:32:54 +0800

    FRONTEND: NEW LOGIN FOR ADMIN

--- frontend_reactJS/src/components/Auth/admin/Login.jsx
+++ frontend_reactJS/src/components/Auth/admin/Login.jsx
@@ -69,38 +69,38 @@ const handleLoginRequestAndResponse = async (event) => {
 
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center drop-shadow-lg">
       <ToastContainer />
       <div className="artboard phone-2 flex flex-col items-center justify-center w-full sm:w-4/5 md:w-2/3 lg:w-1/2 xl:w-1/3 px-4 relative">
         <div className="absolute top-[-0px] left-1/2 transform -translate-x-1/2 z-50">
           <img src="https://i.ibb.co/7JHVynR/DTRMS-LOGO-removebg-preview.png" alt="m4rk" className="min-h-40 min-w-40 sm:w-20 sm:h-20 object-contain" />
         </div>
-        <div className="card shrink-0 w-full shadow-md bg-gradient-to-r from-emerald-500 via-sky-500 to-violet-700 border-t-8 border-b-8 border-black">
+        <div className="card shrink-0 w-full shadow-md bg-gradient-to-r from-emerald-500 via-sky-500 to-violet-700 border-t-8 border-b-8 border-white">
           <form className="card-body" onSubmit={handleLoginRequestAndResponse}>
             <div className="text-center">
-              <h1 className="text-4xl sm:text-5xl font-bold text-black pt-8 sm:pt-10 pb-2">DTRMS+</h1>
+              <h1 className="text-4xl sm:text-5xl font-bold text-white pt-8 sm:pt-10 pb-2">ADMIN</h1>
             </div>
             <div className="form-control">
               <label className="label">
-                <span className="label-text text-lg sm:text-xl text-black">Email</span>
+                <span className="label-text text-lg sm:text-xl text-white">Email</span>
               </label>
               <label className="input input-bordered flex items-center gap-2 glass">
-                <input type="text" value={localEmail} onChange={(e) => setLocalEmail(e.target.value)} className="grow bg-black text-black placeholder-black" placeholder="Email" />
+                <input type="text" value={localEmail} onChange={(e) => setLocalEmail(e.target.value)} className="grow bg-white text-white placeholder-white drop-shadow-lg" placeholder="Email" />
               </label>
             </div>
             <div className="form-control">
               <label className="label">
-                <span className="label-text text-lg sm:text-xl text-black">Password</span>
+                <span className="label-text text-lg sm:text-xl text-white">Password</span>
               </label>
               <label className="input input-bordered flex items-center gap-2 glass">
-                <input value={localPassword} onChange={(e) => setLocalPassword(e.target.value)} type="password" className="grow text-black placeholder-black" placeholder="Password" />
+                <input value={localPassword} onChange={(e) => setLocalPassword(e.target.value)} type="password" className="grow text-white placeholder-white drop-shadow-lg" placeholder="Password" />
               </label>
               <label className="label">
-                <a href="#" className="label-text-alt link link-hover text-black text-sm sm:text-lg">Forgot password?</a>
+                <a href="#" className="label-text-alt link link-hover text-white text-sm sm:text-lg">Forgot password?</a>
               </label>
             </div>
             <div className="form-control mt-6">
-              <button type="submit" className="btn drop-shadow-2xl bg-gradient-to-r from-white to-sky-500 hover:from-white hover:to-violet-800 text-black text-xl sm:text-2xl">Login</button>
+              <button type="submit" className="btn drop-shadow-lg bg-gradient-to-r from-white to-sky-500 hover:from-white hover:to-violet-800 text-black text-xl sm:text-2xl">Login</button>
             </div>
             <center>
               <span id="loading-infinity" className={`loading loading-infinity loading-lg ${isLoading ? 'block' : 'hidden'} spinner-blue`}></span>
